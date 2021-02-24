417c1374938d9e2676330304ac2ec6a68973c252
Ngoc <builamquangngoc91@gmail.com>
2021-02-24 14:59:25 +0700

    crypto

--- internal/storages/sqlstore/user.go
+++ internal/storages/sqlstore/user.go
@@ -4,17 +4,19 @@ import (
 	"context"
 	"database/sql"
 	"github.com/manabie-com/togo/internal/storages/model"
+	"github.com/manabie-com/togo/pkg/common/crypto"
 )
 
 // ValidateUser returns tasks if match userID AND password
 func (s *Store) ValidateUser(ctx context.Context, userID, pwd sql.NullString) bool {
-	stmt := `SELECT id FROM users WHERE id = $1 AND password = $2`
-	row := s.DB.QueryRowContext(ctx, stmt, userID, pwd)
+	stmt := `SELECT id, password FROM users WHERE id = $1`
+	row := s.DB.QueryRowContext(ctx, stmt, userID)
 	u := &model.User{}
-	err := row.Scan(&u.ID)
+
+	err := row.Scan(&u.ID, &u.Password)
 	if err != nil {
 		return false
 	}
 
-	return true
+	return crypto.CheckPasswordHash(pwd.String, u.Password)
 }
