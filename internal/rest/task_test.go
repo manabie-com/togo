98df5c8da465a850b653a3f3a28e29eb5ee46cf7
Phuong Hau <hvphuong98@gmail.com>
2021-03-11 22:33:35 +0700

    todo: integration test

---
+++ internal/rest/task_test.go
@@ -0,0 +1,14 @@
+package rest_test
+
+import (
+	"testing"
+)
+
+func TestLoginHandler(t *testing.T) {
+	// r := http.NewServeMux()
+	// mux := http.NewServeMux()
+	// userRepo := postgresql.NewPSQLUserRepository(db)
+	// taskRepo := postgresql.NewPSQLTaskRespsitory(db)
+	// server := httptest.NewServer()
+
+}
