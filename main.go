29ba1fc1e4b07c0e8ada42456db42fa15d959f29
Phuong Hau <hvphuong98@gmail.com>
2021-03-11 22:33:20 +0700

    modify main with dependencies initialing once, and passing depedencies around

--- main.go
+++ main.go
@@ -2,25 +2,71 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
 	"net/http"
 
+	_ "github.com/lib/pq"
+	"github.com/manabie-com/togo/internal/rest"
 	"github.com/manabie-com/togo/internal/services"
-	sqllite "github.com/manabie-com/togo/internal/storages/sqlite"
-
+	"github.com/manabie-com/togo/internal/storages/postgresql"
 	_ "github.com/mattn/go-sqlite3"
 )
 
+const (
+	host     = "localhost"
+	port     = 8899
+	user     = "phuonghau"
+	password = "phuonghau"
+	dbname   = "togo"
+)
+
+func newPSQLDB() (*sql.DB, error) {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+	db, err := sql.Open("postgres", psqlInfo)
+	return db, err
+}
+
 func main() {
-	db, err := sql.Open("sqlite3", "./data.db")
+	// Initializing postgres connection
+	db, err := newPSQLDB()
 	if err != nil {
-		log.Fatal("error opening db", err)
+		panic(err)
+	}
+	defer db.Close()
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		log.Println("Database connected")
 	}
 
-	http.ListenAndServe(":5050", &services.ToDoService{
-		JWTKey: "wqGyEBBfPK9w3Lxw",
-		Store: &sqllite.LiteDB{
-			DB: db,
-		},
+	// Initialzing dependencies
+	mux := http.NewServeMux()
+	userRepo := postgresql.NewPSQLUserRepository(db)
+	taskRepo := postgresql.NewPSQLTaskRespsitory(db)
+
+	taskService := services.NewTaskService(services.TaskServiceConfiguration{
+		TaskRepo: taskRepo,
 	})
+	authService := services.NewAuthService(services.AuthServiceConfiguration{
+		UserRepo: userRepo,
+		JWTKey:   services.DefaultJWTKey,
+	})
+
+	rest.NewTaskHandler(authService, taskService).Register(mux)
+	rest.NewAuthHandler(authService).Register(mux)
+
+	// HTTP Server configuration
+	address := "0.0.0.0:8080"
+	server := &http.Server{
+		Handler: mux,
+		Addr:    address,
+	}
+
+	// Binding http server to network interface
+	log.Println("Server is listening on", address)
+	log.Fatal(server.ListenAndServe())
 }
