9c654c0f91cce659464e69fd3707ecb5fbce2691
Phuong Hau <hvphuong98@gmail.com>
2021-03-11 22:09:16 +0700

    add repository pattern for db layers, which can be used for any database engine, enable mocking in unit-test

---
+++ internal/storages/storages.go
@@ -0,0 +1,29 @@
+package storages
+
+import (
+	"context"
+	"errors"
+
+	"github.com/manabie-com/togo/internal/entities"
+)
+
+// Implement repository pattern
+// which decouple the storage layer from the business logic layer
+
+// TaskRepository ...
+type TaskRepository interface {
+	SaveTask(ctx context.Context, task entities.Task) (*entities.Task, error)
+	GetTasksByUserIDAndDate(ctx context.Context, userID, createdDate string) ([]*entities.Task, error)
+	CountTasksOfUserByDate(ctx context.Context, userID, createdDate string) (int, error)
+}
+
+// UserRepository ...
+type UserRepository interface {
+	GetUserByUserID(ctx context.Context, userID string) (*entities.User, error)
+}
+
+// Common error
+var (
+	ErrUserNotFound  = errors.New("User not found")
+	ErrInternalError = errors.New("Cannot perform action against database")
+)
