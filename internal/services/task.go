73c345db1b0940667e5a123e74f89e13c6c6727e
Phuong Hau <hvphuong98@gmail.com>
2021-03-12 10:28:26 +0700

    inject user repo to taskservice

--- internal/services/task.go
+++ internal/services/task.go
@@ -26,17 +26,20 @@ var (
 // TaskSvc ...
 type TaskSvc struct {
 	taskRepo storages.TaskRepository
+	userRepo storages.UserRepository
 }
 
 // TaskServiceConfiguration ...
 type TaskServiceConfiguration struct {
 	TaskRepo storages.TaskRepository
+	UserRepo storages.UserRepository
 }
 
 // NewTaskService ...
 func NewTaskService(config TaskServiceConfiguration) *TaskSvc {
 	return &TaskSvc{
 		taskRepo: config.TaskRepo,
+		userRepo: config.UserRepo,
 	}
 }
 
@@ -51,26 +54,34 @@ func (ts *TaskSvc) ListTasksByUserAndDate(ctx context.Context, userID, createdDa
 	// Save to db
 	tasks, err := ts.taskRepo.GetTasksByUserIDAndDate(ctx, userID, createdDate)
 	if err != nil {
-		return nil, errors.New("Internal error")
+		return nil, ErrInternalError
 	}
 	return tasks, nil
 }
 
 // AddTask ...
 func (ts *TaskSvc) AddTask(ctx context.Context, task entities.Task) (*entities.Task, error) {
-	taskCountByDate, err := ts.taskRepo.CountTasksOfUserByDate(ctx, task.UserID, task.CreatedDate)
-	if err != nil {
-		return nil, errors.New("Cannot retrieve info about tasks")
-	}
-
-	if taskCountByDate >= 5 {
-		return nil, ErrTaskLimitOfDayReached
+	if len(task.Content) == 0 {
+		return nil, entities.ErrTaskInvalidContent
 	}
 	task.CreatedDate = time.Now().Format("2006-01-02")
+	taskCountByDate, err := ts.taskRepo.CountTasksOfUserByDate(ctx, task.UserID, task.CreatedDate)
+	if err != nil {
+		return nil, ErrInternalError
+	}
+
+	userTaskLimit, err := ts.userRepo.GetUserTaskLimit(ctx, task.UserID)
+	if err != nil {
+		return nil, ErrInternalError
+	}
+
+	if taskCountByDate >= userTaskLimit {
+		return nil, ErrTaskLimitOfDayReached
+	}
 	task.ID = uuid.New().String()
 	t, err := ts.taskRepo.SaveTask(ctx, task)
 	if err != nil {
-		return nil, errors.New("Cannot save task")
+		return nil, ErrInternalError
 	}
 	return t, nil
 }
