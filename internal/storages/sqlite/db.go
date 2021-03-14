b0cd4aff3daba5c3a2c5fe475d3b6fc5d272fc2d
Danh Banh <danhbanh@chotot.vn>
2021-03-14 18:55:56 +0700

    refactor sqllite store

--- internal/storages/sqlite/db.go
+++ internal/storages/sqlite/db.go
@@ -3,7 +3,7 @@ package sqllite
 import (
 	"context"
 	"database/sql"
-
+	"fmt"
 	"github.com/banhquocdanh/togo/internal/storages"
 )
 
@@ -48,7 +48,7 @@ func (l *LiteDB) RetrieveTasks(ctx context.Context, userID, createdDate string) 
 }
 
 // AddTask adds a new task to DB
-func (l *LiteDB) AddTask(ctx context.Context, t *storages.Task) error {
+func (l *LiteDB) addTask(ctx context.Context, t *storages.Task) error {
 	stmt := `INSERT INTO tasks (id, content, user_id, created_date) VALUES (?, ?, ?, ?)`
 	_, err := l.DB.ExecContext(ctx, stmt, &t.ID, &t.Content, &t.UserID, &t.CreatedDate)
 	if err != nil {
@@ -56,6 +56,53 @@ func (l *LiteDB) AddTask(ctx context.Context, t *storages.Task) error {
 	}
 
 	return nil
+}
+
+func (l *LiteDB) countTaskPerDayByUserID(ctx context.Context, userID string, createdDate string) (uint, error) {
+	stmt := `SELECT COUNT(*) FROM tasks WHERE user_id = ? AND created_date = ?`
+	rows, err := l.DB.QueryContext(ctx, stmt, userID, createdDate)
+	if err != nil {
+		return 0, err
+	}
+	defer rows.Close()
+	var count uint
+	for rows.Next() {
+		if err := rows.Scan(&count); err != nil {
+			fmt.Printf("Err: %s\n", err)
+			return count, err
+		}
+	}
+
+	return count, nil
+}
+
+// AddTask adds a new task to DB
+func (l *LiteDB) AddTask(ctx context.Context, t *storages.Task) error {
+	user, err := l.getUser(ctx, t.UserID)
+	if err != nil {
+		return err
+	}
+	count, err := l.countTaskPerDayByUserID(ctx, user.ID, t.CreatedDate)
+	if err != nil {
+		return err
+	}
+
+	if user.MaxTodo <= count {
+		return fmt.Errorf("max todo task")
+	}
+
+	return l.addTask(ctx, t)
+}
+
+// getUser returns tasks if match with userID
+func (l *LiteDB) getUser(ctx context.Context, userID string) (*storages.User, error) {
+	userIDNotNullString := sql.NullString{String: userID, Valid: true}
+	stmt := `SELECT * FROM users WHERE id = ?`
+	row := l.DB.QueryRowContext(ctx, stmt, userIDNotNullString)
+	u := &storages.User{}
+	err := row.Scan(&u.ID, &u.Password, &u.MaxTodo)
+
+	return u, err
 }
 
 // ValidateUser returns tasks if match userID AND password
