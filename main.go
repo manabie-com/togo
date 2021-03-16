52bee405a5b62bcd24db50dc72f5195cee7eefb0
luuvinhtien <luuvinhtien@gmail.com>
2021-03-16 20:04:14 +0700

    lv-tien
    commit source code

--- main.go
+++ main.go
@@ -2,19 +2,41 @@ package main
 
 import (
 	"database/sql"
-	"log"
+	"fmt"
 	"net/http"
 
 	"github.com/manabie-com/togo/internal/services"
 	sqllite "github.com/manabie-com/togo/internal/storages/sqlite"
 
-	_ "github.com/mattn/go-sqlite3"
+	_ "github.com/lib/pq"
+	//_ "github.com/mattn/go-sqlite3"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = "postgres"
+	dbname   = "lvtien_test"
 )
 
 func main() {
-	db, err := sql.Open("sqlite3", "./data.db")
+	//db, err := sql.Open("sqlite3", "./data.db")
+	//if err != nil {
+	//	log.Fatal("error opening db", err)
+	//}
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+	db, err := sql.Open("postgres", psqlInfo)
 	if err != nil {
-		log.Fatal("error opening db", err)
+		panic(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
 	}
 
 	http.ListenAndServe(":5050", &services.ToDoService{
