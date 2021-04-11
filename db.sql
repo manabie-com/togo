48137e05ff6283646ffebfbd2426465481676c4c
lnghai <lnghai@outlook.com>
2021-04-11 20:42:20 +0700

    Add Postgres docker-compose

---
+++ db.sql
@@ -0,0 +1,14 @@
+CREATE TABLE users (
+    id        TEXT PRIMARY KEY,
+    password  TEXT NOT NULL,
+    max_todo  INTEGER DEFAULT 5 NOT NULL
+);
+
+INSERT INTO users (id, password, max_todo) VALUES ('firstUser', 'example', 5);
+
+CREATE TABLE tasks (
+    id            TEXT PRIMARY KEY,
+    content       TEXT NOT NULL,
+    user_id       TEXT NOT NULL REFERENCES users (id),
+    created_date  TEXT NOT NULL
+);
