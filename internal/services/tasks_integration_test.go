03b0b335f4a58b949440c81d7e56e22becfe7f71
lnghai <lnghai@outlook.com>
2021-04-11 20:50:54 +0700

    Add TestLoginOKPostgres (should fail)

--- internal/services/tasks_integration_test.go
+++ internal/services/tasks_integration_test.go
@@ -1,3 +1,34 @@
 // +build integration
 
 package services
+
+import (
+	"testing"
+	"context"
+
+	"github.com/manabie-com/togo/internal/storages/postgres"
+
+	"github.com/jackc/pgx/v4"
+)
+
+const postgresURL = "postgres://test:test@localhost:5432/test"
+
+// TestLoginOKPostgres call testLoginOK with PostgresDB
+func TestLoginOKPostgres(t *testing.T) {
+	db, err := pgx.Connect(context.TODO(), postgresURL)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer db.Close(context.TODO())
+
+	pg := &postgres.PostgresDB{
+		DB: db,
+	}
+
+	var (
+		user = "firstUser"
+		pass = "example"
+	)
+
+	testLoginOK(t, pg, user, pass)
+}
