0ba40cca434ba84a4234b05029beab7c6c54be9f
lnghai <lnghai@outlook.com>
2021-04-11 23:14:58 +0700

    Check error wrong place

--- internal/services/tasks.go
+++ internal/services/tasks.go
@@ -92,14 +92,6 @@ func (s *ToDoService) listTasks(resp http.ResponseWriter, req *http.Request) {
 	resp.Header().Set("Content-Type", "application/json")
 
 	if err != nil {
-		if errors.Is(err, storages.DailyLimitExceededError) {
-			resp.WriteHeader(http.StatusTooManyRequests)
-			json.NewEncoder(resp).Encode(map[string]string{
-				"error": err.Error(),
-			})
-			return
-		}
-
 		resp.WriteHeader(http.StatusInternalServerError)
 		json.NewEncoder(resp).Encode(map[string]string{
 			"error": err.Error(),
@@ -131,6 +123,14 @@ func (s *ToDoService) addTask(resp http.ResponseWriter, req *http.Request) {
 
 	err = s.Store.AddTask(req.Context(), t)
 	if err != nil {
+		if errors.Is(err, storages.DailyLimitExceededError) {
+			resp.WriteHeader(http.StatusTooManyRequests)
+			json.NewEncoder(resp).Encode(map[string]string{
+				"error": err.Error(),
+			})
+			return
+		}
+
 		resp.WriteHeader(http.StatusInternalServerError)
 		json.NewEncoder(resp).Encode(map[string]string{
 			"error": err.Error(),
