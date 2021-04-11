a92f0cd4c3fc7e8cf6c5b0e53b1006f327801e19
lnghai <lnghai@outlook.com>
2021-04-11 16:27:32 +0700

    Implement mock DB for testing

--- internal/services/tasks_test.go
+++ internal/services/tasks_test.go
@@ -1,1 +1,30 @@
 package services
+
+import (
+	"context"
+	"database/sql"
+
+	"github.com/manabie-com/togo/internal/storages"
+)
+
+// mockDB implements DB interface for testing purpose
+type mockDB struct {
+	mockRetrieveTasks func(ctx context.Context, userID, createdDate sql.NullString) ([]*storages.Task, error)
+	mockAddTask       func(ctx context.Context, t *storages.Task) error
+	mockValidateUser  func(ctx context.Context, userID, pwd sql.NullString) bool
+}
+
+// RetrieveTasks delegates the call to mockRetrieveTasks
+func (db *mockDB) RetrieveTasks(ctx context.Context, userID, createdDate sql.NullString) ([]*storages.Task, error) {
+	return db.mockRetrieveTasks(ctx, userID, createdDate)
+}
+
+// AddTask delegates the call to mockAddTask
+func (db *mockDB) AddTask(ctx context.Context, t *storages.Task) error {
+	return db.mockAddTask(ctx, t)
+}
+
+// ValidateUser delegates the call to mockValidateUser
+func (db *mockDB) ValidateUser(ctx context.Context, userID, pwd sql.NullString) bool {
+	return db.mockValidateUser(ctx, userID, pwd)
+}
