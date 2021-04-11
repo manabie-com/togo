86fa2dcfc9d7de9d636feefaa34d7af04f9fed4f
lnghai <lnghai@outlook.com>
2021-04-11 15:26:45 +0700

    Make ToDoService use DB interface instead of SQLite implementation

--- internal/services/tasks.go
+++ internal/services/tasks.go
@@ -11,13 +11,12 @@ import (
 	jwt "github.com/dgrijalva/jwt-go"
 	"github.com/google/uuid"
 	"github.com/manabie-com/togo/internal/storages"
-	sqllite "github.com/manabie-com/togo/internal/storages/sqlite"
 )
 
 // ToDoService implement HTTP server
 type ToDoService struct {
 	JWTKey string
-	Store  *sqllite.LiteDB
+	Store  storages.DB
 }
 
 func (s *ToDoService) ServeHTTP(resp http.ResponseWriter, req *http.Request) {
