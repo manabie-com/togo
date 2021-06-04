b212bf080728e1300b580f750f5219664f6ada2b
Thienbuu <thienbuu9@gmail.com>
2021-06-04 21:46:14 +0700

    Assignment - Le Gia Thien Buu

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"github.com/manabie-com/togo/internal/services"
 	sqllite "github.com/manabie-com/togo/internal/storages/sqlite"
 
+	// _ "github.com/lib/pq"
 	_ "github.com/mattn/go-sqlite3"
 )
 
@@ -24,3 +25,31 @@ func main() {
 		},
 	})
 }
+
+//Implement postgres instead of sqlite
+// const (
+// 	host     = "localhost"
+// 	port     = 5432
+// 	user     = "postgres"
+// 	password = "postgres"
+// 	dbname   = "postgres"
+// )
+
+// func main() {
+// 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+// 		"password=%s dbname=%s sslmode=disable",
+// 		host, port, user, password, dbname)
+
+// 	db, err := sql.Open("postgres", psqlInfo)
+
+// 	if err != nil {
+// 		log.Fatal("error opening db", err)
+// 	}
+
+// 	http.ListenAndServe(":5050", &services.ToDoService{
+// 		JWTKey: "wqGyEBBfPK9w3Lxw",
+// 		Store: &sqllite.LiteDB{
+// 			DB: db,
+// 		},
+// 	})
+// }
