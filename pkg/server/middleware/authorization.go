ad64d18090f56947108f2c00ab068a7c4574e880
linhtd <linh.tdb@teko.vn>
2021-07-12 13:14:13 +0700

    separate package

--- pkg/server/middleware/authorization.go
+++ pkg/server/middleware/authorization.go
@@ -1,19 +1,11 @@
 package middleware
 
 import (
-	"context"
 	"net/http"
 
+	"github.com/manabie-com/togo/pkg/customcontext"
 	"github.com/manabie-com/togo/pkg/jwtprovider"
 )
-
-type userAuthKey int8
-
-func UserIDFromCtx(ctx context.Context) (string, bool) {
-	v := ctx.Value(userAuthKey(0))
-	id, ok := v.(string)
-	return id, ok
-}
 
 type authMiddleware struct {
 	jwtProvider jwtprovider.JWTProvider
@@ -25,31 +17,35 @@ func NewAuthMiddleware(jwtProvider jwtprovider.JWTProvider) *authMiddleware {
 	}
 }
 
-func (m *authMiddleware) Authorization(next http.Handler) http.Handler {
+func (m *authMiddleware) IsAuthorized(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
-		r, ok := m.validToken(r)
+		token := extractToken(r)
+		userID, ok := m.verifyToken(token)
 		if !ok {
 			w.WriteHeader(http.StatusUnauthorized)
 			return
 		}
+		ctx := customcontext.SetUserIDToContext(r.Context(), userID)
+		r = r.WithContext(ctx)
 		next.ServeHTTP(w, r)
 	})
 }
 
-func (m *authMiddleware) validToken(req *http.Request) (*http.Request, bool) {
-	token := req.Header.Get("Authorization")
+func (m *authMiddleware) verifyToken(token string) (string, bool) {
 
 	payload, valid := m.jwtProvider.Parse(token)
 	if !valid {
-		return req, false
+		return "", false
 	}
 
-	id, ok := payload["user_id"].(string)
+	userID, ok := payload["user_id"].(string)
 	if !ok {
-		return req, false
+		return "", false
 	}
+	return userID, true
+}
 
-	req = req.WithContext(context.WithValue(req.Context(), userAuthKey(0), id))
-	return req, true
+func extractToken(req *http.Request) string {
+	return req.Header.Get("Authorization")
 }
