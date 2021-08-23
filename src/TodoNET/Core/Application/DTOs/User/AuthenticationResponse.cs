194ca548415df1536223323be41371ae481620a5
ThanhCuD <thanhduc552018@gmail.com>
2021-08-23 19:01:18 +0700

    update AuthenModel, Change Folder Name

--- src/TodoNET/Core/Application/DTOs/User/AuthenticationResponse.cs
+++ src/TodoNET/Core/Application/DTOs/User/AuthenticationResponse.cs
@@ -3,7 +3,6 @@
     public class AuthenticationResponse
     {
         public string Id { get; set; }
-        public string UserName { get; set; }
         public string Email { get; set; }
         public string JWToken { get; set; }
     }
