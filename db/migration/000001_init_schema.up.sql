8a8d93b5c26d78f2ae89aa23fba76721ecb2da75
jericogantuangco <jericogantuangco200@gmail.com>
2021-10-18 13:05:54 +0800

    updated db up migration sql script

--- db/migration/000001_init_schema.up.sql
+++ db/migration/000001_init_schema.up.sql
@@ -1,14 +1,16 @@
 CREATE TABLE "users" (
-  "id" bigserial PRIMARY KEY NOT NULL,
+  "username" varchar PRIMARY KEY,
   "password" text NOT NULL,
   "max_todo" integer NOT NULL DEFAULT 5
 );
 
 CREATE TABLE "tasks" (
-  "id" bigserial PRIMARY KEY NOT NULL,
+  "id" bigserial PRIMARY KEY,
   "content" text NOT NULL,
-  "user_id" bigserial NOT NULL,
+  "user_id" varchar NOT NULL,
   "created_date" text NOT NULL
 );
 
-ALTER TABLE "tasks" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
+ALTER TABLE "tasks" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("username");
+
+INSERT INTO users (username, password) VALUES('testUser', 'password')
