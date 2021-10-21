157f9a0bc282f1a5f7b648a64ec87a5e198a50c6
khoahuynhdev <khoa.huynh@unibiz.io>
2021-10-21 22:52:08 +0700

    update comment + cleanup

--- togo-js/src/app.service.ts
+++ togo-js/src/app.service.ts
@@ -4,9 +4,6 @@ import { TaskInput } from './dto/task.create';
 import { Task } from './models/task.entity';
 import { User } from './models/user.entity';
 import { Raw } from 'typeorm';
-// import { InjectRepository } from '@nestjs/typeorm';
-// import { Brackets, Connection, getManager, IsNull, Repository } from 'typeorm';
-// import { JwtPayload } from './services/jwt/strategy';
 
 @Injectable()
 export class AppService {
@@ -29,6 +26,9 @@ export class AppService {
   async createTask(id: number, input: TaskInput) {
     const user = await User.findOne(id);
 
+    // we can just pass in like { ...input }
+    // but we will have to make a global validation and transformation pipe first
+    // i don't have time for that T_T
     const task = Task.create({
       content: input.content || '',
       user: user,
@@ -40,6 +40,8 @@ export class AppService {
   }
 
   async listTasks(id: number) {
+    // i was considering adding index to task.userId column
+    // but it's not worth it
     const tasks = await Task.find({
       where: {
         user: {
