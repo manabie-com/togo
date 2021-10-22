17a5fbdd94f36d6db989c10abd2203c48fdbaf01
khoahuynhdev <khoa.huynh@unibiz.io>
2021-10-22 22:57:12 +0700

    add docker-compose

--- togo-js/src/main.ts
+++ togo-js/src/main.ts
@@ -6,6 +6,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(bodyParser.json({ limit: '50mb' }));
   app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
-  await app.listen(3000);
+  await app.listen(process.env.PORT);
 }
 bootstrap();
