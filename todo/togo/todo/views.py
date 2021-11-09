8530bae62854e0d14670adfc8ea229b5243299ff
gelloguiam <acguiam@gmail.com>
2021-11-09 22:59:48 +0800

    Add handler to save todo date in database
    - Add checker if max tasks for the day is reached
    - Save data if max todo for the day not reached

--- todo/togo/todo/views.py
+++ todo/togo/todo/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.core import serializers
 import json
+from datetime import datetime
 
 from todo.models import User, Task
 
@@ -16,22 +17,34 @@ def login(request):
         password = data.get('password')
 
         user = authenticate(userid, password)
+
         if user:
             request.session['auth'] = True
+            request.session['user'] = user.id
             return HttpResponse('Successfully logged in!')
 
     return HttpResponse('Failed to login!')
 
 
+@csrf_exempt
 def tasks(request):
     if not request.session.has_key('auth') or not request.session['auth']:
         return HttpResponse('You need to login first!')
+    
+    auth_user_id = request.session['user']
+    auth_user = User.objects.get(id=auth_user_id)
 
     if request.method == 'GET':
         date = request.GET.get('created_date')
-        tasks = list(Task.objects.all().filter(date_created = date))
+        date = datetime.strptime(date, '%Y-%m-%d').date()
+
+        tasks = list(Task.objects.all()\
+            .filter(date_created__year = date.year,
+                    date_created__month = date.month,
+                    date_created__day = date.day))
 
         tasks_list = []
+
         for task in list(tasks):
             item = {
                 'content' : task.content,
@@ -40,6 +53,29 @@ def tasks(request):
             tasks_list.append(item)
 
         return JsonResponse(tasks_list, safe=False)
+
+    if request.method == 'POST':
+        data = json.loads(request.body)
+
+        date_today = datetime.today()
+
+        content = data.get('content')
+        no_current_tasks = Task.objects.all()\
+            .filter(user_id = auth_user,
+                date_created__year = date_today.year,
+                date_created__month = date_today.month,
+                date_created__day = date_today.day).count()
+
+        no_max_todo = auth_user.max_todo
+
+        if (no_current_tasks < no_max_todo):
+            task = Task(content = content, user_id = auth_user)
+            task.save()
+
+            return HttpResponse('Successfully saved.')
+        
+        else:
+            return HttpResponse('Max todo for the day already reached.')
  
     return HttpResponse('No data found.')
 
