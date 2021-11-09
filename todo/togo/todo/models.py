f25c921b301d1589effe262c9355e9abe76d35e0
gelloguiam <acguiam@gmail.com>
2021-11-09 22:33:35 +0800

    Restructure database
    - Add incremental PK to user and tasks
    - Fix attribute naming and data types
    - Add intiial data in fixtures foldery

--- todo/togo/todo/models.py
+++ todo/togo/todo/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 
 class User(models.Model):
-    id = models.CharField(primary_key=True, max_length=100, null=False)
+    id = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=100, null=False)
     password = models.CharField(max_length=100, null=False)
-    max_todo = models.IntegerField(null=False)
+    max_todo = models.IntegerField(default=5)
 
 class Task(models.Model):
-    id = models.CharField(primary_key=True, max_length=100, null=False)
+    id = models.AutoField(primary_key=True)
     content = models.CharField(max_length=300, null=False)
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
-    created_date = models.DateField(null=False)
+    date_created = models.DateTimeField(auto_now_add=True)
