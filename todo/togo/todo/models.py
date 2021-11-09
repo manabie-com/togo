b0dcf09b8e17989bf62927c63b1683e4a5471713
gelloguiam <acguiam@gmail.com>
2021-11-09 20:57:47 +0800

    Setup sqlite database and create initial orm models
    - Create models for Users
    - Create models for Tasks

--- todo/togo/todo/models.py
+++ todo/togo/todo/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    id = models.CharField(primary_key=True, max_length=100, null=False)
+    password = models.CharField(max_length=100, null=False)
+    max_todo = models.IntegerField(null=False)
+
+class Task(models.Model):
+    id = models.CharField(primary_key=True, max_length=100, null=False)
+    content = models.CharField(max_length=300, null=False)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_date = models.DateField(null=False)
--- todo/togo/togo/settings.py
+++ todo/togo/togo/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'togo.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'data.db',
     }
 }
 
