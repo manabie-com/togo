37b55b6487647e4d71790cbd762b6c25f42c206d
gelloguiam <acguiam@gmail.com>
2021-11-09 21:43:44 +0800

    Add login function implementation
    - Convert login to POST request
    - Get data from request body
    - Validte matching credentials to user table
    - Return authenticated user object
    - Set session to true

--- todo/togo/todo/views.py
+++ todo/togo/todo/views.py
@@ -1,8 +1,34 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
 
+from todo.models import User, Task
+
+@csrf_exempt
 def login(request):
-    return HttpResponse('Hello World!')
+    if request.method == 'POST':
+        data = json.loads(request.body)
+
+        userid = data.get('user_id')
+        password = data.get('password')
+
+        user = authenticate(userid, password)
+        if user:
+            request.session['auth'] = True
+            return HttpResponse('Successfully logged in!')
+
+    return HttpResponse('Failed to login!')
+
 
 def tasks(request):
+    if not request.session['auth']:
+        return HttpResponse('You need to login first!')
+
     return HttpResponse('Hello World!!')
+
+def authenticate(userid, password):
+    try:
+        return User.objects.get(id=userid, password=password)
+    except:
+        return None
