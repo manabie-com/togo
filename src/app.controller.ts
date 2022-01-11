d07a30c86453251890906aee12691e6721d11db4
nguyenkimbang <sugiaichigo00781@gmail.com>
2022-01-12 05:51:57 +0700

    init db

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,12 @@
-import { Controller, Get } from '@nestjs/common'
+import { Controller, Param, Post } from '@nestjs/common'
 import { AppService } from './app.service'
 
-@Controller()
+@Controller('user')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello()
+  @Post(':userId/task')
+  takeTask(@Body() tasks: string): Promise<any> {
+    return this.appService.takeTask(tasks)
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,39 @@
 import { Injectable } from '@nestjs/common'
+import { join } from 'path'
+import { Low, JSONFile } from 'lowdb'
+import { chain } from 'lodash'
 
+const taskFile = join('src/database/task.json')
+const userFile = join('src/database/user.json')
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!'
+  private readonly taskDB
+  private readonly userDB
+  constructor() {
+    const taskAdapter = new JSONFile(taskFile)
+    const userAdapter = new JSONFile(userFile)
+    this.taskDB = new Low(taskAdapter)
+    this.userDB = new Low(userAdapter)
+  }
+
+  async takeTask(task = ''): Promise<any> {
+    const userId = 2
+    const tasksCount = await this.getUserTasksCount(userId)
+    const user = await this.getUser(userId)
+    return { tasksCount, user }
+  }
+
+  async getUserTasksCount(userId = 0) {
+    await this.taskDB.read()
+    this.taskDB.chain = chain(this.taskDB.data)
+    const userTasks = this.taskDB.chain.find({ assignedUserId: userId }).value()
+    return (userTasks || []).length
+  }
+
+  async getUser(userId = 0) {
+    await this.userDB.read()
+    this.userDB.chain = chain(this.userDB.data)
+    const user = this.userDB.chain.find({ id: userId }).value()
+    return user
   }
 }
