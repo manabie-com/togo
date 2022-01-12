748f165cf85df08a5c9cd4d8a39759c7ed17cf45
nguyenkimbang <sugiaichigo00781@gmail.com>
2022-01-12 18:51:56 +0700

    update README

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,7 +20,7 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
-    'prettier/prettier': ['error', {
+    'prettier/prettier': ['off', {}, {
       'semi': false
     }],
   },
