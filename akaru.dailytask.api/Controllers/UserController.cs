290a2c30a5f0e00623bfa441f1d88633c057cf30
Daniel Rodriguez <daniel.ener.rodriguez@gmail.com>
2022-01-14 20:50:29 +0800

    Added endpoints for generating random users

--- akaru.dailytask.api/Controllers/UserController.cs
+++ akaru.dailytask.api/Controllers/UserController.cs
@@ -18,6 +18,31 @@ namespace akaru.dailytask.api.Controllers
         {
 			return Json(_db.Users.ToList());
         }
+
+        public IActionResult Generate(int num)
+        {
+			var users = Enumerable.Range(0, num).Select(x => GenerateRandomUser());
+			_db.AddRange(users);
+			_db.SaveChanges();
+			return Json(_db.Users.ToList());
+        }
+		public IActionResult Clear()
+        {
+			_db.Users.RemoveRange(_db.Users);
+			_db.SaveChanges();
+			return Redirect("/User");
+        }
+
+        private User GenerateRandomUser()
+        {
+			// Generate User with Random Name and Random DailyTaskLimit
+			int min = 5, max = 20;
+			return new User
+			{
+				Name = Faker.Name.FullName(),
+				DailyTaskLimit = Random.Shared.Next(min, max)
+			};
+        }
 	}
 }
 
