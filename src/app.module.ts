2549b8ce750b32a99ddac645c6400968b1ffdb18
hoaidien93 <hoaidien93@gmail.com>
2022-01-22 17:53:30 +0700

    Add docker + migrate to nest 8

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: '127.0.0.1',
+      port: 3306,
+      username: 'root',
+      password: '1',
+      database: 'togo',
+      synchronize: true,
+      entities: ["dist/**/*.entity{.ts,.js}"],
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ".env"
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
