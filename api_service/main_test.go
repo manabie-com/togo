157efc2c1d6f8552faf742d36025fca2a6c4b728
hoangngth <42316439+hoangngth@users.noreply.github.com>
2022-01-26 19:23:01 +0700

    test template

---
+++ api_service/main_test.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestCreateAccount(t *testing.T) {
+
+}
+
+func TestLoginAccount(t *testing.T) {
+
+}
+
+func TestLogout(t *testing.T) {
+
+}
+
+func TestCreateTodo(t *testing.T) {
+
+}
+
+func TestGetTodo(t *testing.T) {
+
+}
