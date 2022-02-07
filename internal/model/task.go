c39f0bdff7640033674723ae1bec5416af7ad142
jmramos02 <ramosjosemari@gmail.com>
2022-02-07 17:05:25 +0800

    Add data for testing, Fix task json schema

--- internal/model/task.go
+++ internal/model/task.go
@@ -1,9 +1,12 @@
 package model
 
-import "gorm.io/gorm"
+import "time"
 
 type Task struct {
-	*gorm.Model
-	UserID int
-	Name   string
+	ID        uint       `gorm:"primary_key" json:"id"`
+	UserID    int        `json:"user_id"`
+	Name      string     `json:"name"`
+	DeletedAt *time.Time `gorm:"column:deleted_at" json:"deleted_at"`
+	UpdatedAt *time.Time `gorm:"column:updated_at" json:"updated_at"`
+	CreatedAt time.Time  `gorm:"column:created_at" json:"created_at"`
 }
