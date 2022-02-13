a5d1c301a282db03a51c0ebe5db3730e233d61a8
BJ Abichuela <bjabichuela@gmail.com>
2022-02-14 02:14:14 +0800

    Controllers: -- Create Post and Get tasks request

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,5 @@
-const User = require('models/userModel');
-const auth = require('middlewares/auth');
+const User = require('../models/userModel');
+const auth = require('../middlewares/auth');
 const bcrypt = require('bcrypt');
 
 // User login
@@ -7,16 +7,16 @@ module.exports.login = async (req, res) => {
     const user = await User.findOne({ email: req.body.email });
 
     if(user === null){
-        res.send({ auth: 'user does not exist' });
+        res.send({ auth: 'User does not exist' });
     }
     else{
         const isPasswordMatched = bcrypt.compareSync(req.body.password, user.password);
 
         if(isPasswordMatched){
-            res.send({ access: authenticator.createAccessToken(user.toObject()) });
+            res.send({ access: auth.createAccessToken(user.toObject()) });
         }
         else{
-            res.send({ auth: 'login_failed' });
+            res.send({ auth: 'Login failed' });
         }
     }
 }
@@ -26,7 +26,8 @@ module.exports.register = async (req, res) => {
     const user = new User({
         name: req.body.name,
         email: req.body.email,
-        password: bcrypt.hashSync(req.body.password, 10)
+        password: bcrypt.hashSync(req.body.password, 10),
+        isPremium: req.body.isPremium
     });
 
     user.save().then((user, error) => {
@@ -38,4 +39,33 @@ module.exports.register = async (req, res) => {
             res.send(user);
         }
     })
+}
+
+// Add task
+module.exports.addTask = async (req, res) => {
+    const user = await User.findById(req.body.userId);
+
+    user.task.push({ description: req.body.description });
+
+    user.save().then((user, error) => {
+        if(error){
+            res.send(error)
+        }
+        else{
+            res.send({ isAdded: true});
+        }
+    })
+}
+
+// Get task
+module.exports.getTask = async (req, res) => {
+    const user = await User.findById(req.body.userId);
+    let tasks = [];
+
+    user.task.forEach((allTasks) => {
+        if(user){
+            tasks.push(allTasks);
+        }
+    })
+    res.send(tasks)
 }
