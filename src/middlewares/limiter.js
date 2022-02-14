9a2e6ae7e16a7f2d54b66bf2dcad102e5308a2d8
BJ Abichuela <bjabichuela@gmail.com>
2022-02-15 04:34:27 +0800

    Limiter: -- Update limiter

--- src/middlewares/limiter.js
+++ src/middlewares/limiter.js
@@ -1,16 +1,16 @@
 const rateLimit = require("express-rate-limit");
 const User = require("../models/userModel")
 
-module.exports.limiter = rateLimit({
+module.exports.limit = rateLimit({
     windowMs: 24 * 60 * 60 * 1000, // 24 hour window
     max: async (req, res) => {
-        const user = User.findById(req.body.userId)
+        const user = await User.findById(req.body.userId)
         
-        if(user.isPremium){
-            return 10
+        if(user.isPremium === false){
+            return 3
         }else{
             return 5
         }
 	},
-    message: "You have reached the maximum number of requests, please try again after 24 hours"
+    message: "Maximum daily limit reached"
 });
