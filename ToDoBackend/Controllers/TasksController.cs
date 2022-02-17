c15ad97f65b1e0ed131cb90c25c0fa680b338f18
Nguyen Thanh Ha <sherlock.nth@gmail.com>
2022-02-17 12:14:10 +0700

    Implement maximum N tasks per day limit logic.

--- ToDoBackend/Controllers/TasksController.cs
+++ ToDoBackend/Controllers/TasksController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -21,10 +22,10 @@ namespace ToDoBackend.Controllers
         }
 
         [HttpGet]
-        public async Task<IActionResult> Get()
+        public async Task<IActionResult> Get([FromQuery] string userId)
         {
             _logger.LogInformation("Reading all tasks from database...");
-            return Ok(await _dbContext.Tasks.ToListAsync());
+            return Ok(await _dbContext.Tasks.Where(x => x.UserId == userId).ToListAsync());
         }
 
         [HttpPatch]
@@ -44,15 +45,43 @@ namespace ToDoBackend.Controllers
         public async Task<IActionResult> Post([FromBody] Models.Task values)
         {
             _logger.LogInformation("Creating a new task...");
+
+            if (string.IsNullOrEmpty(values.UserId))
+            {
+                return Unauthorized();
+            }
+
+            var user = await _dbContext.Users.FindAsync(values.UserId);
+            if (user is null)
+            {
+                return Unauthorized();
+            }
+
+            await _dbContext.Entry(user).Reference(x => x.Settings).LoadAsync();
+            var today = DateTimeOffset.Now.Date;
+            if (user.Settings != null)
+            {
+                var tasksCount = await _dbContext.Tasks
+                    .Where(task => task.UserId == user.Id && task.CreatedDate == today)
+                    .CountAsync();
+
+                if (tasksCount >= user.Settings.MaxTasksPerDay)
+                {
+                    _logger.LogInformation("Number of tasks reaches maximum limit per day for user ID {UserId}.", user.Id);
+                    return BadRequest("Number of tasks reaches maximum limit per day.");
+                }
+            }
+
             var entry = _dbContext.Tasks.Add(new Models.Task
             {
                 Id = Guid.NewGuid().ToString(),
                 Content = values.Content,
                 Status = values.Status,
-                UserId = values.UserId,
-                CreatedDate = DateTimeOffset.Now,
+                UserId = user.Id,
+                CreatedDate = DateTimeOffset.Now.Date,
             });
             await _dbContext.SaveChangesAsync();
+
             return Ok(entry.Entity);
         }
 
