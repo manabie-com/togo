2d19ddd6e0d3080a2ed2055cf5ff0fd9896781b3
Phan Phuc Tho <phanphuctho7760@gmail.com>
2022-02-24 00:01:21 +0700

    Add readme Add script create database

--- togo-db.sql
+++ togo-db.sql
@@ -1,34 +1,114 @@
--- CREATE SCHEMA `togo` DEFAULT CHARACTER SET utf8 ;
+-- MySQL dump 10.13  Distrib 8.0.28, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: togo
+-- ------------------------------------------------------
+-- Server version	8.0.28
 
-CREATE TABLE `todos` (
-  `id` varchar(45) NOT NULL,
-  `user_id` varchar(45) NOT NULL,
-  `description` varchar(45) DEFAULT NULL,
-  `status` varchar(45) DEFAULT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id_UNIQUE` (`id`)
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+--
+-- Table structure for table `todo_limit`
+--
+
+DROP TABLE IF EXISTS `todo_limit`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `todo_limit` (
-  `id` varchar(45) NOT NULL,
-  `user_id` varchar(45) NOT NULL,
-  `limit` int DEFAULT 0,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id_UNIQUE` (`id`)
-);
+    `id` VARCHAR(45) NOT NULL,
+    `user_id` VARCHAR(45) NOT NULL,
+    `limit` INT DEFAULT '0',
+    `created_at` TIMESTAMP NULL DEFAULT NULL,
+    `updated_at` TIMESTAMP NULL DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `id_UNIQUE` (`id`)
+)  ENGINE=INNODB DEFAULT CHARSET=UTF8MB3;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `todo_limit`
+--
+
+LOCK TABLES `todo_limit` WRITE;
+/*!40000 ALTER TABLE `todo_limit` DISABLE KEYS */;
+INSERT INTO `todo_limit` VALUES ('5d383d63-19e6-40d9-b773-73aeef607abb','6ac34862-4322-4437-9f98-e87fb6e8371b',3,'2022-02-23 16:25:38','2022-02-23 16:25:38');
+/*!40000 ALTER TABLE `todo_limit` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `todos`
+--
+
+DROP TABLE IF EXISTS `todos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `todos` (
+    `id` VARCHAR(45) NOT NULL,
+    `user_id` VARCHAR(45) NOT NULL,
+    `description` VARCHAR(45) DEFAULT NULL,
+    `status` VARCHAR(45) DEFAULT NULL,
+    `created_at` TIMESTAMP NULL DEFAULT NULL,
+    `updated_at` TIMESTAMP NULL DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `id_UNIQUE` (`id`)
+)  ENGINE=INNODB DEFAULT CHARSET=UTF8MB3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `todos`
+--
+
+LOCK TABLES `todos` WRITE;
+/*!40000 ALTER TABLE `todos` DISABLE KEYS */;
+INSERT INTO `todos` VALUES ('4416249c-18f2-405d-bc49-814b4e30c5d5','6ac34862-4322-4437-9f98-e87fb6e8371b','Complete API login user','new','2022-02-23 16:25:38','2022-02-23 16:25:38');
+/*!40000 ALTER TABLE `todos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `users` (
-  `id` varchar(45) NOT NULL,
-  `username` varchar(45) DEFAULT NULL,
-  `password` varchar(45) DEFAULT NULL,
-  `name` varchar(100) DEFAULT NULL,
-  `email` varchar(45) DEFAULT NULL,
-  `status` varchar(45) DEFAULT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`)
-);
+    `id` VARCHAR(45) NOT NULL,
+    `username` VARCHAR(45) DEFAULT NULL,
+    `password` VARCHAR(45) DEFAULT NULL,
+    `name` VARCHAR(100) DEFAULT NULL,
+    `email` VARCHAR(45) DEFAULT NULL,
+    `status` VARCHAR(45) DEFAULT NULL,
+    `created_at` TIMESTAMP NULL DEFAULT NULL,
+    `updated_at` TIMESTAMP NULL DEFAULT NULL,
+    PRIMARY KEY (`id`)
+)  ENGINE=INNODB DEFAULT CHARSET=UTF8MB3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES ('b9ba5650-9b11-4dd5-af43-dbe6c8ada290','6ac34862-4322-4437-9f98-e87fb6e8371b',NULL,'Tho','email@email','active','2022-02-23 16:25:38','2022-02-23 16:25:38');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-02-23 23:27:52
