832364750714cdd156a6e2094fed76105609e13d
Juan Gabriel Cabaña <jcabana@excelliscorp.com>
2022-02-27 18:21:28 +0800

    Updated .gitignore, added project files, created unit test stubs.

---
+++ TODO/TODO.Api/Controllers/WeatherForecastController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TODO.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetSomething()
+        {
+            try
+            {
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e.Message);
+                return StatusCode(500, e.Message);
+            }
+        }
+    }
+}
---
+++ TODO/TODO.UnitTests/TodoControllerTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Extensions.Logging;
+using Moq;
+using TODO.Api.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace TODO.UnitTests
+{
+    [TestClass]
+    public class TodoControllerTests
+    {
+        [TestMethod]
+        public async Task TestRun()
+        {
+            // Arrange 
+            var logger = Mock.Of<ILogger<WeatherForecastController>>();
+            var weatherForecastController = new WeatherForecastController(logger);
+
+            // Act
+            var result = await weatherForecastController.GetSomething();
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(OkResult));
+        }
+    }
+}
