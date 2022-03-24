cdf162ce40f348bbc7a3892d90dfe6eef81244db
earvin-roldan <earvin.roldan@yahoo.com>
2022-03-24 20:09:37 +0800

    Initial Commit

---
+++ src/main/java/com/manabie/togo/TogoApplication.java
@@ -0,0 +1,13 @@
+package com.manabie.togo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TogoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TogoApplication.class, args);
+	}
+
+}
