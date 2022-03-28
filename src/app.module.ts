5081c8c091d4003e28ba7a8b82ca8fa8e290822b
Phuc <phucld@e-plus.vn>
2022-03-29 00:58:46 +0700

    feat: init project with nestjs

---
+++ src/app.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { TodoModule } from './todo/todo.module';
+console.log('process.env', process.env)
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: process.env.DB_DIALECT,
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      synchronize: true,
+      autoLoadEntities: true,
+    }),
+    TodoModule,
+  ],
+  providers: [],
+})
+export class AppModule {}
---
+++ src/todo/todo.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { Todo } from './entities/todo.entity';
+import { TodoService } from './todo.service';
+import { TodoController } from './todo.controller';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Todo])],
+  controllers: [TodoController],
+  providers: [TodoService]
+})
+export class TodoModule {}
