d7d7b8e859b5145396b4520bf296e8fd7488e7af
Truong Phan <me@truongphan.dev>
2022-03-28 19:55:51 +0700

    add domain error test

---
+++ domain/domain_suite_test.go
@@ -0,0 +1,13 @@
+package domain_test
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+func TestDomain(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Domain Suite")
+}
