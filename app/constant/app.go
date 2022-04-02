bfd39a99c0aeaa0bc6a238e9974c5b6d1a00eaba
Le Minh Tri <ansidev@gmail.com>
2022-04-03 02:00:04 +0700

    feat(package): add app package `constant`

---
+++ app/constant/app.go
@@ -0,0 +1,11 @@
+package constant
+
+const (
+	DefaultProdEnv    = "prod"
+	DefaultProdConfig = "app.env.example"
+	DefaultDevConfig  = "app.env"
+
+	AppName        = "ToGo API Project"
+	AppVersion     = "1.0.0"
+	AppReleaseDate = "2022.04.02"
+)
