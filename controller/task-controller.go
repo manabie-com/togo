75003b7afffea0d4b6b8ff86b8d8bd778c60ccdf
qgdomingo <quennelgiodomingo@gmail.com>
2022-04-15 15:02:15 +0800

    Fixed separation of files

---
+++ controller/task-controller.go
@@ -0,0 +1,155 @@
+package controller
+
+import (
+	"net/http"
+	"strconv"
+	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v4/pgxpool"
+	"github.com/qgdomingo/todo-app/repository"
+	"github.com/qgdomingo/todo-app/model"
+)
+
+type TaskDB struct {
+	DBPoolConn *pgxpool.Pool
+}
+
+func (db *TaskDB) GetTasks(c *gin.Context) {
+	allTasks, errMessage := repository.GetTasksDB(db.DBPoolConn, nil)
+	
+	if errMessage != nil {
+		c.IndentedJSON(http.StatusInternalServerError, errMessage)
+		return
+	}
+
+	if len(allTasks) == 0 {
+		c.IndentedJSON(http.StatusNotFound, gin.H{ "message" : "No tasks data were found" })
+	} else {
+		c.IndentedJSON(http.StatusOK, allTasks)
+	}
+}
+
+func (db *TaskDB) GetTaskById(c *gin.Context) {
+	id := c.Param("id")
+	
+	if id, err := strconv.Atoi(id); err == nil  {
+		allTasks, errMessage := repository.GetTasksDB(db.DBPoolConn, id)
+
+		if errMessage != nil {
+			c.IndentedJSON(http.StatusInternalServerError, errMessage)
+			return
+		}
+
+		if len(allTasks) == 0 {
+			c.IndentedJSON(http.StatusNotFound, gin.H{ "message" : "No data was found for the specified id" })
+		} else {
+			c.IndentedJSON(http.StatusOK, allTasks[0])
+		}
+
+	} else {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{ "message" : "Invalid ID was entered on the request URL" })
+	}
+}
+
+func (db *TaskDB) GetTaskByUser(c *gin.Context) {
+	user := c.Param("user")
+	
+	if user != "" {
+		allTasks, errMessage := repository.GetTasksDB(db.DBPoolConn, user)
+
+		if errMessage != nil {
+			c.IndentedJSON(http.StatusInternalServerError, errMessage)
+			return
+		}
+
+		if len(allTasks) == 0 {
+			c.IndentedJSON(http.StatusNotFound, gin.H{ "message" : "No data was found for the specified username" })
+		} else {
+			c.IndentedJSON(http.StatusOK, allTasks)
+		}
+
+	} else {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{ "message" : "No username was entered on the request URL" })
+	}
+}
+
+func (db *TaskDB) CreateTask(c *gin.Context) {
+	var taskDetails model.TaskUserEnteredDetails
+
+	err := c.ShouldBindJSON(&taskDetails)
+
+	if err != nil {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{
+			"message" : "Error on binding data from request",
+			"error"   : err.Error() })
+	}
+
+	isTaskCreated, errMessage := repository.InsertTaskDB(db.DBPoolConn, &taskDetails)
+
+	if errMessage != nil {
+		c.IndentedJSON(http.StatusInternalServerError, err)
+		return
+	}
+
+	if isTaskCreated {
+		c.IndentedJSON(http.StatusOK, gin.H{ "message" : "New task has been created successfully" })
+	} else {
+		c.IndentedJSON(http.StatusOK, gin.H{ "message" : "Task has not been created due to the user reaching the new task limit today" })
+	}
+}
+
+func (db *TaskDB) UpdateTask(c *gin.Context) {
+	id := c.Param("id")
+	
+	if id, err := strconv.Atoi(id); err == nil {
+		var taskDetails model.TaskUserEnteredDetails
+
+		err := c.ShouldBindJSON(&taskDetails)
+
+		if err != nil {
+			c.IndentedJSON(http.StatusBadRequest, gin.H{
+				"message" : "Error on binding data from request",
+				"error"   : err.Error() })
+		}
+	
+		isTaskUpdated, errMessage := repository.UpdateTaskDB(db.DBPoolConn, &taskDetails, id)
+
+		if errMessage != nil {
+			c.IndentedJSON(http.StatusInternalServerError, errMessage)
+			return
+		}
+	
+		if isTaskUpdated {
+			c.IndentedJSON(http.StatusOK, gin.H{ "message" : "Task has been updated successfully" })
+		} else {
+			c.IndentedJSON(http.StatusOK, gin.H{ "message" : "Task was not updated, task with the provided id and/or username is not found." })
+		}
+	
+	} else {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{ "message" : "Invalid ID was entered on the request URL" })
+	}
+
+}
+
+func (db *TaskDB) DeleteTask(c *gin.Context) {
+	id := c.Param("id")
+	
+	if id, err := strconv.Atoi(id); err == nil {
+	
+		isTaskDeleted, errMessage := repository.DeleteTaskDB(db.DBPoolConn, id)
+
+		if errMessage != nil {
+			c.IndentedJSON(http.StatusInternalServerError, errMessage)
+			return
+		}
+	
+		if isTaskDeleted {
+			c.IndentedJSON(http.StatusOK, gin.H{ "message" : "Task has been removed successfully" })
+		} else {
+			c.IndentedJSON(http.StatusOK, gin.H{ "message" : "Task was not removed, task with the provided id is not found." })
+		}
+		
+	} else {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{ "message" : "Invalid ID was entered on the request URL" })
+	}
+
+}
---
+++ repository/task-repo.go
@@ -0,0 +1,110 @@
+package repository
+
+import (
+	"context"
+	"github.com/jackc/pgx/v4"
+	"github.com/jackc/pgx/v4/pgxpool"
+	"github.com/qgdomingo/todo-app/model"
+)
+
+
+func GetTasksDB (dbPoolConn *pgxpool.Pool, searchParam any) ([]model.Task, map[string]string) {
+	var allTasks []model.Task
+	var rows pgx.Rows
+	var err error
+	message := make(map[string]string)
+	sql := "SELECT id, title, description, username, create_date from tasks"
+
+	if searchParam != nil {
+		switch searchParam.(type) {
+			case int:
+				sql += " where id = $1"
+			case string:
+				sql += " where username = $1"
+			default:
+				message["message"] = "Search task parameter entered is invalid"
+				message["error"] = ""
+				return nil, message
+		}
+
+		rows, err = dbPoolConn.Query(context.Background(), sql, searchParam)
+	} else {
+		rows, err = dbPoolConn.Query(context.Background(), sql)
+	}
+
+	if err != nil {
+		message["message"] = "Unable to fetch data from tasks table"
+		message["error"] = err.Error()
+		return nil, message
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+		var task model.Task
+		err = rows.Scan(&task.ID, &task.Title, &task.Description, &task.Username, &task.CreateDate)
+		if err != nil {
+			message["message"] = "Error encountered when row data is being fetched"
+			message["error"] = err.Error()
+			return nil, message
+		}
+		allTasks = append(allTasks, task)
+	}
+
+	if rows.Err() != nil {
+		message["message"] = "Error encountered when accesssing query results"
+		message["error"] = rows.Err().Error()
+		return nil, message
+	}
+
+	return allTasks, nil
+}
+
+
+func InsertTaskDB (dbPoolConn *pgxpool.Pool, task *model.TaskUserEnteredDetails) (bool, map[string]string) {
+	message := make(map[string]string)
+	sql := "INSERT INTO tasks (title, description, username) SELECT $1, $2, $3::VARCHAR FROM task_config WHERE name = 'task_limit' AND value::INTEGER > (SELECT COUNT(id) FROM tasks WHERE username = $3 AND create_date = current_date) RETURNING id"
+
+	row, err := dbPoolConn.Query(context.Background(), sql, task.Title, task.Description, task.Username)
+	if err != nil {
+		message["message"] = "Unable to insert data into the tasks table"
+		message["error"] = err.Error()
+		return false, message
+	}
+
+	defer row.Close()
+
+	return row.Next(), nil
+}
+
+func UpdateTaskDB (dbPoolConn *pgxpool.Pool, task *model.TaskUserEnteredDetails, id int) (bool, map[string]string) {
+	message := make(map[string]string)
+	sql := "UPDATE tasks SET title = $1, description = $2 WHERE username = $3 AND id = $4 RETURNING id"
+
+	row, err := dbPoolConn.Query(context.Background(), sql, task.Title, task.Description, task.Username, id)
+	if err != nil {
+		message["message"] = "Unable to update data into the tasks table"
+		message["error"] = err.Error()
+		return false, message
+	}
+
+	defer row.Close()
+
+	return row.Next(), nil
+}
+
+func DeleteTaskDB (dbPoolConn *pgxpool.Pool, id int) (bool, map[string]string) {
+	message := make(map[string]string)
+	sql := "DELETE FROM tasks WHERE id = $1 RETURNING id"
+
+	row, err := dbPoolConn.Query(context.Background(), sql, id)
+	if err != nil {
+		message["message"] = "Unable to delete data from the tasks table"
+		message["error"] = err.Error()
+		return false, message
+	}
+
+	defer row.Close()
+
+	return row.Next(), nil
+}
