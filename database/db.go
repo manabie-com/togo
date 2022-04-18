6f25b1d2f10c2ed328be77a5aa413d083c9c4e49
qgdomingo <quennelgiodomingo@gmail.com>
2022-04-18 17:32:49 +0800

    Updated to environment variable of db connection string

--- database/db.go
+++ database/db.go
@@ -1,14 +1,13 @@
 package database
 
 import (
-	// "os"
+	"os"
 	"context"
 	"github.com/jackc/pgx/v4/pgxpool"
 )
 
 func CreateConnection() (*pgxpool.Pool, error) {
-	// connectionString := os.Getenv("POSTGRES_DB_URL")
-	connectionString := "postgres://todo_user:secret@localhost:5432/tododb"
+	connectionString := os.Getenv("POSTGRES_DB_URL")
 
 	dbpool, err := pgxpool.Connect(context.Background(), connectionString)
 	
