f242630c5fab1c0fcb9d33b56d7b1070f3981b7f
thanhpn6 <v.thanhpn6@vinfast.vn>
2022-04-25 17:12:44 +0700

    -Add spring boot project -Add basic logic: connect to AWS and try to upload file to S3

---
+++ PostCelebMoment/src/main/java/com/manabie/postcelebmoment/PostCelebMomentApplication.java
@@ -0,0 +1,82 @@
+package com.manabie.postcelebmoment;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Date;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+
+@SpringBootApplication
+public class PostCelebMomentApplication {
+	
+	private AmazonS3 s3client;
+
+	@Value("${amazonProperties.endpointUrl}")
+	private String endpointUrl;
+	@Value("${amazonProperties.bucketName}")
+	private String bucketName;
+	@Value("${amazonProperties.accessKey}")
+	private String accessKey;
+	@Value("${amazonProperties.secretKey}")
+	private String secretKey;
+	@Value("${amazonProperties.region}")
+	private String region;
+
+	@PostConstruct
+	private void initializeAmazon() {
+		BasicAWSCredentials creds = new BasicAWSCredentials(this.accessKey, this.secretKey); 
+		this.s3client = AmazonS3ClientBuilder.standard().withCredentials(new AWSStaticCredentialsProvider(creds)).withRegion(this.region).build();
+	}
+	
+	
+	private File convertMultiPartToFile(MultipartFile file) throws IOException {
+	    File convFile = new File(file.getOriginalFilename());
+	    FileOutputStream fos = new FileOutputStream(convFile);
+	    fos.write(file.getBytes());
+	    fos.close();
+	    return convFile;
+	}
+	
+	private String generateFileName(MultipartFile multiPart) {
+	    return new Date().getTime() + "-" + multiPart.getOriginalFilename().replace(" ", "_");
+	}
+	
+	private void uploadFileTos3bucket(String fileName, File file) {
+	    s3client.putObject(new PutObjectRequest(bucketName, fileName, file)
+	            .withCannedAcl(CannedAccessControlList.PublicRead));
+	}
+	
+	public String uploadFile(MultipartFile multipartFile) {
+
+	    String fileUrl = "";
+	    try {
+	        File file = convertMultiPartToFile(multipartFile);
+	        String fileName = generateFileName(multipartFile);
+	        fileUrl = endpointUrl + "/" + bucketName + "/" + fileName;
+	        uploadFileTos3bucket(fileName, file);
+	        file.delete();
+	    } catch (Exception e) {
+	       e.printStackTrace();
+	    }
+	    return fileUrl;
+	}
+	
+	
+	public static void main(String[] args) {
+		SpringApplication.run(PostCelebMomentApplication.class, args);
+	}
+	
+}
---
+++ PostCelebMoment/src/test/java/com/manabie/postcelebmoment/PostCelebMomentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manabie.postcelebmoment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostCelebMomentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
