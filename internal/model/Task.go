1487a02a68db66b152c7ad276c8321dd881c7ad0
kierquebs <40307377+kierquebs@users.noreply.github.com>
2022-04-26 14:49:11 +0800

    add validation

--- internal/model/Task.go
+++ internal/model/Task.go
@@ -4,8 +4,8 @@ import "gorm.io/gorm"
 
 type Task struct {
 	gorm.Model
-	Name        string `json:"name"`
-	Description string `json:"description"`
-	UserID      uint   `json:"user_id"`
+	Name        string `json:"name" validate:"required,min=3,max=32"`
+	Description string `json:"description" validate:"required,min=3,max=32"`
+	UserID      uint   `json:"user_id" validate:"required,number"`
 	User        User   `gorm:"foreignKey:UserID;references:ID" json:"user"`
 }
