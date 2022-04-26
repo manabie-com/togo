7be3d74856cd0d1abaeb73a18d654862d93ddd3c
kierquebs <40307377+kierquebs@users.noreply.github.com>
2022-04-26 14:50:49 +0800

    add password encrypting

--- internal/services/user/Create.go
+++ internal/services/user/Create.go
@@ -1,14 +1,36 @@
-package services
+package user
 
 import (
 	"togo/internal/connect"
 	"togo/internal/model"
+	"togo/internal/utils"
+
+	"github.com/go-playground/validator/v10"
+	"github.com/gofiber/fiber/v2"
 )
 
-func Create() int64 {
+func Create(c *fiber.Ctx) error {
 
-	user := model.User{UserName: "test", Password: "test", MaxTodos: 10}
+	payload := new(model.User)
+	utils.BodyParser(c, payload)
+	validate := validator.New()
 
-	result := connect.DB.Create(&user)
-	return result.RowsAffected
+	if err := validate.Struct(payload); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(err.Error())
+	}
+
+	hash, err := utils.HashPassword(payload.Password)
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(err.Error())
+	}
+
+	payload.Password = hash
+
+	result := connect.DB.Create(&payload)
+	if result.Error != nil {
+		return c.Status(500).JSON(fiber.Map{"retCode": 500, "message": result.Error})
+
+	}
+
+	return c.JSON(payload)
 }
