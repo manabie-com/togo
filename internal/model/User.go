a9e445585b102fe304bdf11b77fcb5acdf42dad6
kierquebs <40307377+kierquebs@users.noreply.github.com>
2022-04-26 14:49:30 +0800

    modify fields

--- internal/model/User.go
+++ internal/model/User.go
@@ -4,7 +4,7 @@ import "gorm.io/gorm"
 
 type User struct {
 	gorm.Model
-	UserName string `json:"user_name"`
-	Password string `json:"password"`
-	MaxTodos int    `json:"max_todos"`
+	UserName string `json:"user_name" `
+	Password string `json:"password" `
+	MaxTodos int    `json:"max_todos" `
 }
