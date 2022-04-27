8d67fc0e5f09556b5758f20f9d2b1b3a7f8780d2
kierquebs <40307377+kierquebs@users.noreply.github.com>
2022-04-27 15:29:45 +0800

    implement jwt authentication using middleware.Protected()

--- internal/route/TaskRoutes.go
+++ internal/route/TaskRoutes.go
@@ -1,6 +1,7 @@
 package route
 
 import (
+	"togo/internal/middleware"
 	"togo/internal/services/task"
 
 	"github.com/gofiber/fiber/v2"
@@ -9,7 +10,7 @@ import (
 func TaskRoutes(rg fiber.Router) {
 
 	tg := rg.Group("/task")
-	tg.Post("/create", task.Create)
+	tg.Post("/create", middleware.Protected(), task.Create)
 
 	//rg.PUT("/task/:id", task.Update)
 	//rg.DELETE("/task/:id", task.Delete)
