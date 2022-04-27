be34b4a9922d0085c38edf5ac486a7292d5a92ef
kierquebs <40307377+kierquebs@users.noreply.github.com>
2022-04-27 15:30:53 +0800

    add query select to get user credentials

--- internal/services/login/Validate.go
+++ internal/services/login/Validate.go
@@ -1,5 +1,27 @@
 package login
 
-func Validate(uname string, pass string) bool {
+import (
+	"fmt"
+	"togo/internal/connect"
+	"togo/internal/model"
+	"togo/internal/utils"
+)
 
+var user model.User
+
+func Validate(uname string, pass string) bool {
+	connect.DB.Table("users").Select("user_name", "password").Where("user_name = ?", uname).Scan(&user)
+	fmt.Println(fmt.Sprintf("username: %s", user.UserName))
+	fmt.Println(fmt.Sprintf("password: %s", user.Password))
+
+	fmt.Println(user.Password)
+	if user.UserName == "" {
+		return false
+	}
+
+	if utils.CheckPasswordHash(pass, user.Password) {
+		return true
+	}
+
+	return false
 }
