4d74be3c82a31cf6cb10b950db35e1f8471123b9
Le Hoang Long <le.hoang.long@outlook.com>
2022-04-29 02:44:41 +0800

    Added repository factory sql

--- backend/internal/repositories/repository_factory_sql.go
+++ backend/internal/repositories/repository_factory_sql.go
@@ -3,11 +3,88 @@ package repositories
 import (
 	"database/sql"
 	"sync"
+	"context"
+	"manabie.com/internal/common"
 )
 
-type RepositoryFactoriesFactory struct {
+type RepositoryFactory struct {
 	db *sql.DB
 	transactions map[TransactionId]*sql.Tx
-	mu sync.Mutex
+	mtx sync.Mutex
 	count int
+}
+
+func MakeRepositoryFactory(
+	iDb *sql.DB,
+) RepositoryFactory {
+	return RepositoryFactory {
+		db: iDb,
+		count: 0,
+		transactions: map[TransactionId]*sql.Tx{},
+	}
+}
+
+func isolationLevelToSqlIsolationLevel(iLevel TransactionLevel) sql.IsolationLevel {
+	switch iLevel {
+	case ReadUncommitted:
+		return sql.LevelReadUncommitted
+	case ReadCommitted:
+		return sql.LevelReadCommitted
+	case RepeatableRead:
+		return sql.LevelRepeatableRead
+	case Serializable:
+		return sql.LevelSerializable
+	}
+	return sql.LevelReadCommitted
+}
+
+func (f *RepositoryFactory) StartTransactionAuto(
+	iContext context.Context, 
+	iIsolationLevel TransactionLevel,
+	iHandler TransactionHandler,
+) error {
+	tx, err := f.db.BeginTx(iContext, &sql.TxOptions{Isolation: isolationLevelToSqlIsolationLevel(iIsolationLevel)})
+	if err != nil {
+		return err
+	}
+	f.mtx.Lock()
+	count := TransactionId(f.count)
+	f.transactions[count] = tx
+	f.count += 1
+	f.mtx.Unlock()
+	err = iHandler(count)
+
+	f.mtx.Lock()
+	delete(f.transactions, count)
+	f.mtx.Unlock()
+
+	if err != nil {
+		tx.Rollback()
+	} else {
+		tx.Commit()
+	}
+
+	return err
+}
+
+func (f *RepositoryFactory) GetTaskRepository(iId TransactionId) (TaskRepositoryI, error) {
+	f.mtx.Lock()
+	defer f.mtx.Unlock()
+	if tx, ok := f.transactions[iId]; !ok {
+		return TaskRepositorySql{}, common.NotFound
+	} else {
+		repo := MakeTaskRepositorySql(tx)
+		return repo, nil
+	}
+}
+
+func (f *RepositoryFactory) GetUserRepository(iId TransactionId) (UserRepositoryI, error) {
+	f.mtx.Lock()
+	defer f.mtx.Unlock()
+	if tx, ok := f.transactions[iId]; !ok {
+		return UserRepositorySql{}, common.NotFound
+	} else {
+		repo := MakeUserRepositorySql(tx)
+		return repo, nil
+	}
 }
---
+++ backend/internal/repositories/repository_factory_sql_test.go
@@ -0,0 +1,158 @@
+package repositories
+
+import (
+	"context"
+	"database/sql"
+	"github.com/stretchr/testify/require"
+	"testing"
+	"fmt"
+	"manabie.com/internal/common"
+	"manabie.com/internal/models"
+	"time"
+)
+
+func verifyTaskCount(expected int, db *sql.DB, t *testing.T) {
+	result, err := db.Query(`SELECT COUNT(*) FROM "tasks" WHERE user_id=1`)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer result.Close()
+	result.Next()
+	count := 0
+	result.Scan(&count)
+	require.Equal(t, expected, count)
+	result.Close()
+}
+
+func TestRepositoryFactorySql(t *testing.T) {
+	db := ConnectPostgres()
+	defer db.Close()
+
+
+	t.Run("can create repositories", func (t *testing.T) {
+		factory := MakeRepositoryFactory(db)
+		ctx := context.Background()
+		testCreateRepositories := func (oDone chan error) {
+			err := factory.StartTransactionAuto(
+				ctx, 
+				ReadCommitted,
+				func (iTransactionId TransactionId) error  {
+					time.Sleep(500 * time.Millisecond)
+					_, err := factory.GetTaskRepository(iTransactionId)
+					if err != nil {
+						return err
+					}
+
+					time.Sleep(500 * time.Millisecond)
+					_, err = factory.GetUserRepository(iTransactionId)
+					if err != nil {
+						return err
+					}
+
+					return nil
+				},
+			)
+			require.Equal(t, nil, err)
+			oDone <- nil
+		}
+
+		/// create multiple transactions simultaneously
+		numberOfTx := 50
+		channels := make([]chan error, 0, numberOfTx)
+		for i := 0; i < numberOfTx; i++ {
+			channel := make(chan error)
+			go testCreateRepositories(channel)
+			channels = append(channels, channel)
+		}
+
+		for i := 0; i < numberOfTx; i++ {
+			res := <- channels[i]
+			require.Equal(t, nil, res)
+		}
+	})
+
+	t.Run("commit if ok", func (t *testing.T) {
+		factory := MakeRepositoryFactory(db)
+		SetUpTaskRepositorySqlTest(db)
+
+		verifyTaskCount(0, db, t)
+
+		ctx := context.Background()
+		err := factory.StartTransactionAuto(
+			ctx, 
+			ReadCommitted,
+			func (iTransactionId TransactionId) error  {
+				repository, err := factory.GetTaskRepository(iTransactionId)
+				if err != nil {
+					t.Fatal(err)
+				}
+
+				user := models.MakeUser(1, "test-user-1", 2)
+				tasks := []models.Task{}
+
+				for i := 0; i < 1; i++ {
+					tasks = append(tasks, models.MakeTask(
+						-1,
+						fmt.Sprintf("title-%d",i),
+						fmt.Sprintf("content-%d",i),
+						common.MakeTime(time.Unix(1, 0)),
+						nil,
+					))
+				}
+				tasks, err = repository.CreateTaskForUser(ctx, user, tasks)
+				if err != nil {
+					t.Fatal(err)
+				}
+
+				require.Equal(t, 1, len(tasks))
+				return nil
+			},
+		)
+		verifyTaskCount(1, db, t)
+		require.Equal(t, nil, err)
+	})
+
+
+	t.Run("rollback if error", func (t *testing.T) {
+		factory := MakeRepositoryFactory(db)
+		SetUpTaskRepositorySqlTest(db)
+
+		verifyTaskCount(0, db, t)
+
+		ctx := context.Background()
+		mockError := fmt.Errorf("error")
+		err := factory.StartTransactionAuto(
+			ctx, 
+			ReadCommitted,
+			func (iTransactionId TransactionId) error  {
+				repository, err := factory.GetTaskRepository(iTransactionId)
+				if err != nil {
+					t.Fatal(err)
+				}
+
+				user := models.MakeUser(1, "test-user-1", 2)
+				tasks := []models.Task{}
+
+				for i := 0; i < 1; i++ {
+					tasks = append(tasks, models.MakeTask(
+						-1,
+						fmt.Sprintf("title-%d",i),
+						fmt.Sprintf("content-%d",i),
+						common.MakeTime(time.Unix(1, 0)),
+						nil,
+					))
+				}
+				tasks, err = repository.CreateTaskForUser(ctx, user, tasks)
+				if err != nil {
+					t.Fatal(err)
+				}
+
+				require.Equal(t, 1, len(tasks))
+				return mockError
+			},
+		)
+
+		verifyTaskCount(0, db, t)
+		require.Equal(t, mockError, err)
+	})
+}
