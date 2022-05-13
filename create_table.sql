aed67ed687cae629a583f613df6515b65ae5355d
Ivan Platon <platon.ivan@yahoo.com>
2022-05-13 19:56:58 +0800

    Update create_table.sql

--- create_table.sql
+++ create_table.sql
@@ -1,5 +1,16 @@
-CREATE ROLE testuser CREATEDB WITH LOGIN PASSWORD 'P@55w0rd'; 
-CREATE DATABASE testdb;
+CREATE TABLE IF NOT EXISTS users (
+   user_id varchar(255) not null,
+   name varchar(255),
+   task_per_day int,
+   created_at timestamp DEFAULT now(),
+   updated_at timestamp DEFAULT now(),
+   CONSTRAINT users_PK PRIMARY KEY (user_id)
+);
+
+INSERT INTO users (user_id, name, task_per_day) VALUES 
+('USER_1', 'Juan Dela Cruz', 3),
+('USER_2', 'Pedro De Coco', 5);
+('USER_3', 'LeBron James', 10);
 
 CREATE TABLE IF NOT EXISTS tasks (
    id UUID not null,
@@ -7,18 +18,7 @@ CREATE TABLE IF NOT EXISTS tasks (
    title varchar(255),
    description varchar(255),
    created_at timestamp DEFAULT now(),
-   updated_at timestamp DEFAULT now()
+   updated_at timestamp DEFAULT now(),
+   CONSTRAINT tasks_PK PRIMARY KEY (id),
+   CONSTRAINT users_id_FKEY FOREIGN KEY (assigned_to) REFERENCES users(user_id)
 );
-
-CREATE TABLE IF NOT EXISTS users (
-   user_id varchar(255) not null,
-   name varchar(255),
-   task_per_day int,
-   created_at timestamp DEFAULT now(),
-   updated_at timestamp DEFAULT now()
-);
-
-INSERT INTO users (user_id, name, task_per_day) VALUES 
-('USER_1', 'Juan Dela Cruz', 3),
-('USER_2', 'Pedro De Coco', 5);
-('USER_3', 'LeBron James', 10);
