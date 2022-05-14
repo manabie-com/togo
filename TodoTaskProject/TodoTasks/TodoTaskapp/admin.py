4224b096416e4db1bb4ee64ee7ef86e26d48c642
hoangvanmanh34 <66060657+hoangvanmanh34@users.noreply.github.com>
2022-05-14 07:14:59 +0700

    Drop to re-upload

--- TodoTaskProject/TodoTasks/TodoTaskapp/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import UserTaskAllow, UserRecordTask
-
-# Register your models here.
-
-admin.site.register(UserRecordTask)
-admin.site.register(UserTaskAllow)
--- TodoTaskProject/TodoTasks/TodoTaskapp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class TodotaskappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'TodoTaskapp'
