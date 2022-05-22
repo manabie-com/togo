08821b0c90e4bde3028b869f9e82daa228e6cbf9
RAFFY PERALTA <raffy.peralta.01@gmail.com>
2022-05-22 21:09:23 +0800

    Add task db migration

---
+++ db/migrate/20220522125857_create_tasks.rb
@@ -0,0 +1,11 @@
+class CreateTasks < ActiveRecord::Migration[7.0]
+  def change
+    create_table :tasks do |t|
+      t.string :title
+      t.text :body
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
