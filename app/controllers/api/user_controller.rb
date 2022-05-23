a33f4f5a1df7857563222cf850e234e9025e5dd2
RAFFY PERALTA <raffy.peralta.01@gmail.com>
2022-05-23 13:45:32 +0800

    Added error message for user api

--- app/controllers/api/user_controller.rb
+++ app/controllers/api/user_controller.rb
@@ -11,7 +11,7 @@ module Api
       if user.save
         render json:user, status: :created
       else
-        render status: :unprocessable_entity
+        render json: user.errors, status: :unprocessable_entity
       end
     end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  has_many :tasks
+
+  validates :task_limit, presence: true
 end
