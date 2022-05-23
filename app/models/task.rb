2cc9cff2a4d1ed3a3cfae76688af3f0625353e77
RAFFY PERALTA <raffy.peralta.01@gmail.com>
2022-05-23 14:01:18 +0800

    Added validation for Task model

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,13 @@
 class Task < ApplicationRecord
   belongs_to :user
+
+  validates :title, presence: true
+  validates :body, presence: true
+  validate :user_limit
+  
+  def user_limit
+    if  user.tasks.where(created_at: Time.zone.now.beginning_of_day..Time.zone.now).length >= user.task_limit
+      errors.add(:user_limit, message: "Task limit has been reached for today.")
+    end
+  end
 end
