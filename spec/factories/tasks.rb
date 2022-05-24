8b7231134818412c41f669197800585505a4399e
RAFFY PERALTA <raffy.peralta.01@gmail.com>
2022-05-24 14:09:40 +0800

    Add factories needed for testing

---
+++ spec/factories/tasks.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :task do
+      title { "title" }
+      body { "body" }
+  end
+end
---
+++ spec/factories/users.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :user do
+    name { 'name '}
+  end
+end
