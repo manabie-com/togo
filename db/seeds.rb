c4df48684843d90ff2f8cef9ac2b95a69105b23d
RAFFY PERALTA <raffy.peralta.01@gmail.com>
2022-05-25 13:53:19 +0800

    Add seed for db data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+3.times do
+  user = User.new(name: Faker::Games::Dota.hero, task_limit: rand(1..3))
+
+  if user.save
+    Task.create(
+      title: Faker::Book.title,
+      body: Faker::Lorem.sentence,
+      user_id: user.id
+    )
+  end
+end
