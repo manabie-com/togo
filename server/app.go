06b9b78aad4b0ede48aecccb53285a85a1a01e7a
Bogs Espinas <jrpespinas@gmail.com>
2022-05-30 23:22:44 +0800

    feat: establish initial connection to mongodb container

--- server/app.go
+++ server/app.go
@@ -1,15 +1,40 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"net/http"
 	"os"
+
+	lr "togo/utils/logger"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "hello\n")
-	})
+	// Set logging
+	logger := lr.NewLogger(os.Getenv("LOG_LEVEL"))
 
-	http.ListenAndServe(fmt.Sprintf(":%v", os.Getenv("PORT")), nil)
+	// Create a new client and connect to the server
+	mongodb_uri := fmt.Sprintf("%v:%v", os.Getenv("MONGODB_URI"), os.Getenv("DATABASE_PORT"))
+
+	// Set client options
+	clientOptions := options.Client().ApplyURI(mongodb_uri)
+
+	// Connect to MongoDB
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+	logger.Info().Msgf("Connecting to database %v", mongodb_uri)
+
+	if err != nil {
+		logger.Fatal().Err(err).Msg("Connection failed")
+	}
+
+	// Check the connection
+	err = client.Ping(context.TODO(), nil)
+
+	if err != nil {
+		logger.Fatal().Err(err).Msg("Ping failed")
+	}
+
+	logger.Info().Msg("Connected to database")
 }
--- server/go.mod
+++ server/go.mod
@@ -3,6 +3,11 @@ module togo
 go 1.18
 
 require (
+	github.com/rs/zerolog v1.26.1
+	go.mongodb.org/mongo-driver v1.9.1
+)
+
+require (
 	github.com/go-stack/stack v1.8.0 // indirect
 	github.com/golang/snappy v0.0.1 // indirect
 	github.com/klauspost/compress v1.13.6 // indirect
@@ -11,8 +16,7 @@ require (
 	github.com/xdg-go/scram v1.0.2 // indirect
 	github.com/xdg-go/stringprep v1.0.2 // indirect
 	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
-	go.mongodb.org/mongo-driver v1.9.1 // indirect
-	golang.org/x/crypto v0.0.0-20201216223049-8b5274cf687f // indirect
-	golang.org/x/sync v0.0.0-20190911185100-cd5d95a43a6e // indirect
-	golang.org/x/text v0.3.5 // indirect
+	golang.org/x/crypto v0.0.0-20211215165025-cf75a172585e // indirect
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
+	golang.org/x/text v0.3.6 // indirect
 )
