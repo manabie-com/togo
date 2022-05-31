9672c552c5491e0af0a53d4fdf26908f74dc8490
Bogs Espinas <jrpespinas@gmail.com>
2022-05-31 22:58:18 +0800

    feat: implement Task controller

---
+++ server/controller/response/response.go
@@ -0,0 +1,13 @@
+package response
+
+type ErrorResponse struct {
+	Status  string      `json:"status"`
+	Code    int         `json:"code"`
+	Message interface{} `json:"message"`
+}
+
+type SuccessResponse struct {
+	Status string      `json:"status"`
+	Code   int         `json:"code"`
+	Data   interface{} `json:"message"`
+}
---
+++ server/controller/task_controller.go
@@ -0,0 +1,65 @@
+package controller
+
+import (
+	"encoding/json"
+	"net/http"
+	"togo/controller/response"
+	"togo/models"
+	"togo/service"
+)
+
+type TaskController interface {
+	CreateTask(w http.ResponseWriter, r *http.Request)
+}
+
+type taskcontroller struct {
+	taskservice service.TaskService
+}
+
+// Define a Constructor
+// Dependency Injection for Task Controller
+func NewTaskController(service service.TaskService) TaskController {
+	return &taskcontroller{
+		taskservice: service,
+	}
+}
+
+// Create a task record
+// Route: POST /tasks
+// Access: protected
+func (c *taskcontroller) CreateTask(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	// Get POST body
+	var task models.Task
+	err := json.NewDecoder(r.Body).Decode(&task)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(response.ErrorResponse{
+			Status:  "Fail",
+			Code:    http.StatusInternalServerError,
+			Message: err.Error(),
+		})
+	}
+
+	// Validate Task
+	err = c.taskservice.Validate(&task)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(response.ErrorResponse{
+			Status:  "Fail",
+			Code:    http.StatusBadRequest,
+			Message: err.Error(),
+		})
+	}
+
+	// Create Task
+	c.taskservice.Create(&task)
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(response.SuccessResponse{
+		Status: "Success",
+		Code:   http.StatusOK,
+		Data:   task,
+	})
+
+}
