7ec16f9e95f8640b669ad5986e49b5efb63bd9bc
Bogs Espinas <jrpespinas@gmail.com>
2022-05-31 22:58:46 +0800

    refactor: change datatype of the models

--- server/models/task.go
+++ server/models/task.go
@@ -2,14 +2,12 @@ package models
 
 import (
 	"time"
-
-	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type Task struct {
-	TaskID      primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
-	Title       string             `json:"title,omitempty" bson:"title,omitempty"`
-	Description string             `json:"description,omitempty" bson:"description,omitempty"`
-	CreatedAt   time.Time          `json:"created_at,omitempty" bson:"created_at,omitempty"`
-	CreatedBy   primitive.ObjectID `json:"user_id,omitempty" bson:"user_ids,omitempty"`
+	TaskID      string    `json:"id,omitempty" bson:"_id,omitempty"`
+	Title       string    `json:"title,omitempty" bson:"title,omitempty"`
+	Description string    `json:"description,omitempty" bson:"description,omitempty"`
+	CreatedAt   time.Time `json:"created_at,omitempty" bson:"created_at,omitempty"`
+	CreatedBy   string    `json:"user_id,omitempty" bson:"user_ids,omitempty"`
 }
--- server/models/user.go
+++ server/models/user.go
@@ -1,14 +1,10 @@
 package models
 
-import (
-	"go.mongodb.org/mongo-driver/bson/primitive"
-)
-
 type User struct {
-	ID       primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
-	Email    string             `json:"email,omitempty" bson:"email, omitempty"`
-	Password string             `json:"password,omitempty" bson:"password, omitempty"`
-	Token    string             `json:"token,omitempty" bson:"token, omitempty"`
-	Limit    int                `json:"limit,omitempty" bson:"limit, omitempty"`
-	Tasks    []Task             `json:"limit,omitempty" bson:"limit, omitempty"`
+	ID       string `json:"id,omitempty" bson:"_id,omitempty"`
+	Email    string `json:"email,omitempty" bson:"email, omitempty"`
+	Password string `json:"password,omitempty" bson:"password, omitempty"`
+	Token    string `json:"token,omitempty" bson:"token, omitempty"`
+	Limit    int    `json:"limit,omitempty" bson:"limit, omitempty"`
+	Tasks    []Task `json:"tasks,omitempty" bson:"tasks, omitempty"`
 }
