e41f37f7bf8d04a243863004333097411e2aefad
Bogs Espinas <jrpespinas@gmail.com>
2022-06-01 23:38:38 +0800

    feat: implement User controller

---
+++ server/controller/user_controller.go
@@ -0,0 +1,127 @@
+package controller
+
+import (
+	"encoding/json"
+	"net/http"
+	"time"
+	"togo/controller/response"
+	"togo/models"
+	"togo/service"
+)
+
+type UserController interface {
+	Register(w http.ResponseWriter, r *http.Request)
+	Login(w http.ResponseWriter, r *http.Request)
+}
+
+type usercontroller struct {
+	userservice service.UserService
+}
+
+// Define a Constructor
+// Dependency Injection for Task Controller
+func NewUserController(service service.UserService) UserController {
+	return &usercontroller{
+		userservice: service,
+	}
+}
+
+// Create a task record
+// Route: POST /register
+// Access: public
+func (c *usercontroller) Register(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	// Get POST body
+	var user models.User
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(response.ErrorResponse{
+			Status:  "Fail",
+			Code:    http.StatusInternalServerError,
+			Message: err.Error(),
+		})
+	}
+
+	// Validate User
+	err = c.userservice.ValidateRegistration(&user)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(response.ErrorResponse{
+			Status:  "Fail",
+			Code:    http.StatusBadRequest,
+			Message: err.Error(),
+		})
+	}
+
+	// Register User
+	c.userservice.Register(&user)
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(response.SuccessResponse{
+		Status: "Success",
+		Code:   http.StatusOK,
+		Data:   user.Email,
+	})
+}
+
+// Create a task record
+// Route: POST /register
+// Access: public
+func (c *usercontroller) Login(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	// Get POST body
+	var user models.User
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(response.ErrorResponse{
+			Status:  "Fail",
+			Code:    http.StatusInternalServerError,
+			Message: err.Error(),
+		})
+	}
+
+	// Validate User Login
+	err = c.userservice.ValidateLogin(&user)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(response.ErrorResponse{
+			Status:  "Fail",
+			Code:    http.StatusBadRequest,
+			Message: err.Error(),
+		})
+	}
+
+	// Set expiration time of the token
+	expiration := time.Now().Add(time.Minute * 60)
+
+	// Generate JWT token
+	token, err := c.userservice.GenerateJWT(&user, expiration)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(response.ErrorResponse{
+			Status:  "Fail",
+			Code:    http.StatusBadRequest,
+			Message: err.Error(),
+		})
+	}
+
+	// Set cookie
+	http.SetCookie(w,
+		&http.Cookie{
+			Name:     "token",
+			Value:    token,
+			Expires:  expiration,
+			HttpOnly: true,
+		})
+
+	// Lgoin
+	c.userservice.Login(&user)
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(response.SuccessResponse{
+		Status: "Success",
+		Code:   http.StatusOK,
+	})
+}
