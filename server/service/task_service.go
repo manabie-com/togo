3d13462d928c144ebb4648418fb18963f000ba5c
Bogs Espinas <jrpespinas@gmail.com>
2022-06-03 00:30:37 +0800

    feat: define new functions and inject User repository

--- server/service/task_service.go
+++ server/service/task_service.go
@@ -4,8 +4,8 @@ import (
 	"errors"
 	"time"
 	"togo/models"
-
-	repository "togo/repository/task"
+	taskrepo "togo/repository/task"
+	userrepo "togo/repository/user"
 
 	"github.com/rs/xid"
 )
@@ -18,19 +18,29 @@ type TaskService interface {
 
 	// Generate an ID and set creation time
 	Create(task *models.Task) (*models.Task, error)
+
+	// Assign `User` to `Task`
+	GetUser(token string, task *models.Task) error
+
+	// Check if `User` exceeds limited number of tasks per day
+	GetLimit(token string) error
 }
 
 // Define struct with `TaskRepository` as the attribute
 // This attribute is responsible for `Task` database interactions
 type taskservice struct {
-	taskrepository repository.TaskRepository
+	taskrepository taskrepo.TaskRepository
+	userrepository userrepo.UserRepository
 }
 
 // Define a Constructor
 // Dependency Injection for `Task` Service
-func NewTaskService(repo repository.TaskRepository) TaskService {
+func NewTaskService(
+	taskrepository taskrepo.TaskRepository,
+	userrepository userrepo.UserRepository) TaskService {
 	return &taskservice{
-		taskrepository: repo,
+		taskrepository: taskrepository,
+		userrepository: userrepository,
 	}
 }
 
@@ -58,4 +68,40 @@ func (s *taskservice) Create(task *models.Task) (*models.Task, error) {
 	task.CreatedAt = time.Now()
 
 	return s.taskrepository.CreateTask(task)
+}
+
+// Retrieve `User` by token
+func (s *taskservice) GetUser(token string, task *models.Task) error {
+	// Check if session exists
+	user, err := s.userrepository.GetUserByToken(token)
+	if err != nil {
+		return errors.New("session not found")
+	}
+
+	// Assign `User` to `Task`
+	task.CreatedBy = user.ID
+	return nil
+}
+
+// Retrieve user by token
+func (s *taskservice) GetLimit(token string) error {
+	// Get current date
+	now := time.Now()
+
+	// Get `User` limit
+	user, _ := s.userrepository.GetUserByToken(token)
+
+	// Get number of `Tasks` by `User` by current date
+	count, err := s.taskrepository.CountTask(user.ID, now)
+	if err != nil {
+		return err
+	}
+
+	// Check if limit reached
+	if count < user.Limit {
+		return nil
+	} else {
+		return errors.New("number of maximum tasks reached")
+	}
+
 }
