d3e2c4574fae8d7a414049180aa9c93e395351bd
Bogs Espinas <jrpespinas@gmail.com>
2022-06-03 00:29:59 +0800

    feat: implement sessions and limit checking in the controller

--- server/controller/task_controller.go
+++ server/controller/task_controller.go
@@ -60,6 +60,51 @@ func (c *taskcontroller) CreateTask(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	// Get `User` session
+	cookie, err := r.Cookie("token")
+	if err != nil {
+		if err == http.ErrNoCookie {
+			w.WriteHeader(http.StatusUnauthorized)
+			json.NewEncoder(w).Encode(response.ErrorResponse{
+				Status:  "fail",
+				Code:    http.StatusUnauthorized,
+				Message: err.Error(),
+			})
+			return
+		}
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(response.ErrorResponse{
+			Status:  "fail",
+			Code:    http.StatusBadRequest,
+			Message: err.Error(),
+		})
+		return
+	}
+
+	// Check `User.Limit` if adding task is allowed
+	err = c.taskservice.GetLimit(cookie.Value)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(response.ErrorResponse{
+			Status:  "fail",
+			Code:    http.StatusInternalServerError,
+			Message: err.Error(),
+		})
+		return
+	}
+
+	// Assign `User` to `Task`
+	err = c.taskservice.GetUser(cookie.Value, &task)
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		json.NewEncoder(w).Encode(response.ErrorResponse{
+			Status:  "fail",
+			Code:    http.StatusUnauthorized,
+			Message: err.Error(),
+		})
+		return
+	}
+
 	// Create Task
 	c.taskservice.Create(&task)
 	w.WriteHeader(http.StatusOK)
