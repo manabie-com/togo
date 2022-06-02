1df766f83977672c7e66b7082bca9b8158dfc3df
Bogs Espinas <jrpespinas@gmail.com>
2022-06-03 00:32:31 +0800

    feat: implement CountTask method

--- server/repository/task/mongo_task_repository.go
+++ server/repository/task/mongo_task_repository.go
@@ -2,10 +2,12 @@ package repository
 
 import (
 	"context"
+	"log"
 	"os"
 	"time"
 	"togo/models"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -28,4 +30,33 @@ func (db *mongodb) CreateTask(task *models.Task) (*models.Task, error) {
 	defer cancel()
 	_, err := collection.InsertOne(ctx, task)
 	return task, err
+}
+
+// Find all tasks per current day per user
+func (db *mongodb) CountTask(userid string, now time.Time) (int, error) {
+	collection := db.conn.Database(os.Getenv("DATABASE_NAME")).Collection("task")
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	// Define filter
+	year, month, day := now.Date()
+	today := time.Date(year, month, day, 0, 0, 0, 0, now.Location())
+	log.Println(userid)
+	filter := bson.D{
+		{"created_by", userid},
+		{"created_at", bson.D{{"$gte", today}}},
+	}
+
+	cursor, err := collection.Find(ctx, filter)
+	if err != nil {
+		return 0, err
+	}
+	var found []models.Task
+	// var found []bson.M
+	if err := cursor.All(context.TODO(), &found); err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println(len(found))
+	return len(found), nil
 }
--- server/repository/task/tasks_repository.go
+++ server/repository/task/tasks_repository.go
@@ -1,10 +1,16 @@
 package repository
 
-import "togo/models"
+import (
+	"time"
+	"togo/models"
+)
 
 // Define `Task` Repository Interface with the following
 // Methods which will be utilized by the `UserService`
 type TaskRepository interface {
 	// Add a new task in the database
 	CreateTask(task *models.Task) (*models.Task, error)
+
+	// Get the number of `Tasks` for current date
+	CountTask(token string, now time.Time) (int, error)
 }
