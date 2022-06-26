411ab09910d0325d0d4ce1e140ee9f256e513832
DuongThanhTin <tinduong245@gmail.com>
2022-06-26 14:54:17 +0700

    Set middleware and set repository

--- cmd/main.go
+++ cmd/main.go
@@ -4,7 +4,10 @@ import (
 	"fmt"
 	"log"
 
+	"example.com/m/v2/cmd/middlewares"
 	"example.com/m/v2/constants"
+	"example.com/m/v2/internal/api/handlers"
+	"example.com/m/v2/internal/api/routes"
 	"example.com/m/v2/internal/driver"
 	"example.com/m/v2/utils"
 
@@ -28,6 +31,22 @@ func run() error {
 	r.Use(func(c *gin.Context) {
 		c.Set("db", db)
 	})
+
+	{ // Set Middlewares
+		// Midlewares
+		r.Use(gin.Logger())
+		// Recovery middleware recovers from any panics and writes a 500 if there was one.
+		r.Use(gin.Recovery())
+		r.Use(middlewares.SetDefaultMiddleWare())
+		r.Use(middlewares.ValidateToken())
+	}
+
+	{ //Set Repository
+		usecases := handlers.NewUseCase(db)
+		handlers.SetMainUseCase(&usecases)
+		//Set Route
+		routes.SetupRoute(r, usecases)
+	}
 
 	log.Fatal(r.Run(fmt.Sprintf(":%s", utils.Env.Port)))
 	return nil
