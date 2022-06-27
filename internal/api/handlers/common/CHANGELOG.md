1557cdb5b1c3fab3f58fc3c2d6725bdf989d59cc
“DuongThanhTin” <tinduong245@gmail.com>
2022-06-27 12:38:44 +0700

    Add unit test in handler

--- internal/api/handlers/common/CHANGELOG.md
+++ internal/api/handlers/common/CHANGELOG.md
@@ -7,5 +7,6 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ### Added
 
+- Add unit test
 - Make create `router`
 - Handle func common
--- internal/api/handlers/tasks/task_test.go
+++ internal/api/handlers/tasks/task_test.go
@@ -13,7 +13,6 @@ import (
 	"github.com/manabie-com/togo/constants"
 	"github.com/manabie-com/togo/internal/api/handlers"
 	"github.com/manabie-com/togo/internal/models"
-	"github.com/manabie-com/togo/internal/repositories/task"
 	"github.com/manabie-com/togo/internal/usecases/authorization"
 	"github.com/manabie-com/togo/utils"
 	"github.com/stretchr/testify/assert"
@@ -54,11 +53,11 @@ func TestAddTask(t *testing.T) {
 		err := recordStatsUser(db, user.Username, user.Password)
 		require.Nil(t, err)
 
-		input := task.NewTask(models.Task{
+		input := models.Task{
 			Content:    "Test Interview",
 			CreateDate: time.Now().Format("2006-01-02"),
 			UserID:     "1",
-		})
+		}
 
 		rows := sqlmock.NewRows([]string{"id", "content", "create_date", "user_id"}).
 			AddRow("1", input.Content, input.CreateDate, 1)
@@ -88,5 +87,55 @@ func TestAddTask(t *testing.T) {
 		w := httptest.NewRecorder()
 		r.ServeHTTP(w, req)
 		assert.Equal(t, http.StatusCreated, w.Code)
+	}
+
+	{ //Fail case
+		// Create mock data User for test
+		user := models.User{
+			Username: "manabie-test-3",
+			Password: "123456",
+		}
+		mock.ExpectExec("INSERT INTO users").
+			WithArgs(1, user.Username, user.Password, 5).
+			WillReturnResult(sqlmock.NewResult(1, 1))
+
+		// Create Data
+		err := recordStatsUser(db, user.Username, user.Password)
+		require.Nil(t, err)
+
+		input := models.Task{
+			Content:    "Test Interview",
+			CreateDate: time.Now().Format("2006-01-02"),
+			UserID:     "1",
+		}
+
+		rows := sqlmock.NewRows([]string{"id", "content", "create_date", "user_id"}).
+			AddRow("1", input.Content, input.CreateDate, 1)
+
+		mock.ExpectQuery(regexp.QuoteMeta(`SELECT * FROM "tasks" WHERE (user_id = $1 and create_date = $2)`)).
+			WithArgs(input.UserID, input.CreateDate).
+			WillReturnRows(rows)
+
+		mock.ExpectBegin()
+		mock.ExpectQuery(regexp.QuoteMeta(`INSERT INTO "tasks" ("id","content","create_date","user_id") VALUES ($1,$2,$3,$4) RETURNING "tasks"."id"`)).
+			WithArgs(input.ID, input.Content, input.CreateDate, input.UserID).
+			WillReturnRows(sqlmock.NewRows([]string{"id"}).AddRow(1))
+		mock.ExpectCommit()
+
+		//Generate token
+		token, err := authUsecase.GenerateToken(input.UserID, "0")
+		require.Nil(t, err)
+
+		jsonValue, _ := json.Marshal(input)
+		req, _ := http.NewRequest(http.MethodPost, "/tasks", bytes.NewBuffer(jsonValue))
+		cookie := &http.Cookie{
+			Name:   constants.CookieTokenKey,
+			Value:  utils.SafeString(token),
+			MaxAge: 300,
+		}
+		req.AddCookie(cookie)
+		w := httptest.NewRecorder()
+		r.ServeHTTP(w, req)
+		assert.Equal(t, http.StatusInternalServerError, w.Code)
 	}
 }
