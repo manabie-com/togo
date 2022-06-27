96f91377c1853fd45f6da09b9cb6ff4038b1a869
Huynh Huu Loc <huynhhuuloc129@gmail.com>
2022-06-27 15:45:22 +0700

    Update day 5

--- test/validateinput_test.go
+++ test/validateinput_test.go
@@ -8,6 +8,34 @@ import (
 	"github.com/huynhhuuloc129/todo/models"
 )
 
+func TestCheckUserInput(t *testing.T) {
+	user1 := models.NewUser{
+		Username: "",
+		Password: "",
+	}
+	user2 := models.NewUser{
+		Username: "",
+		Password: "asdfasf",
+	}
+	user3 := models.NewUser{
+		Username: "asdfasf",
+		Password: "",
+	}
+	user4 := models.NewUser{
+		Username: "asdfasfa",
+		Password: "asdfsafsf",
+	}
+	result1 := models.CheckUserInput(user1)
+	result2 := models.CheckUserInput(user2)
+	result3 := models.CheckUserInput(user3)
+	result4 := models.CheckUserInput(user4)
+
+	if result1 || result2 || result3 || !result4 {
+		log.Fatal("Check user input failed")
+	}
+
+}
+
 func TestCheckTaskInput(t *testing.T) {
 	task1 := models.NewTask{
 		Content:  "sadfsaf",
