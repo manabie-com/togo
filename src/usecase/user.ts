a01eca2207d4487d54c635e852d661c8a3d17a67
vincent villaluna <villalunavincent@gmail.com>
2022-07-20 16:40:07 +0800

    Add usecase handler for authentication

--- src/usecase/user.ts
+++ src/usecase/user.ts
@@ -1,8 +1,11 @@
 import { UserRequest } from '../dto/userRequest';
 import { UserResponse } from '../dto/userResponse';
+import { AuthResponse } from '../dto/authResponse';
+import { AuthRequest } from '../dto/authRequest';
 
 interface UserUsecase {
 	create(u: UserRequest): Promise<UserResponse>
+	authenticate(u: AuthRequest): Promise<AuthResponse>
 }
 
 export default UserUsecase;
--- src/usecase/userUsecase.ts
+++ src/usecase/userUsecase.ts
@@ -1,17 +1,36 @@
 import UserRepo from '../repo/user';
 import { UserRequest } from '../dto/userRequest';
+import { AuthRequest } from '../dto/authRequest';
 import { User } from '../entity/User';
-import { hash } from '../util/hasher';
+import { hash, compare } from '../util/hasher';
+import { jwt } from '../util/jwt';
+import { Error } from '../error';
 
 const userUsecase = (repo: UserRepo) => ({
 	create: async (u: UserRequest) => {
-		u.validate()
+		u.validate();
 		const user = new User();
 		user.username = u.username;
 		user.password = await hash(u.password);
 		user.type = u.type;
 		const userResponse = await repo.create(user);
-		return userResponse
+		return userResponse;
+	},
+	authenticate: async (u: AuthRequest) => {
+		u.validate();
+		const user = new User();
+		user.username = u.username;
+		user.password = u.password;
+		const { username, id, type, password } = await repo.authenticate(user);
+
+		const claims = await compare(u.password, password);
+
+		if (claims) {
+			return { token: jwt.generate({ id, username, type }) }
+		}
+
+		Error.exec('User not found on database', 401);
+		return { token: '' };
 	}
 })
 
