f98ed572c5c66806b75f7f13dda34a2e4fb62361
vincent villaluna <villalunavincent@gmail.com>
2022-07-20 14:30:06 +0800

    Handle database error and provide custom error response

--- src/repo/userRepo.ts
+++ src/repo/userRepo.ts
@@ -1,14 +1,21 @@
 import { DataSource } from 'typeorm';
 import { User } from '../entity/User';
-import userResponse, { UserResponse } from '../dto/userResponse';
+import userResponse from '../dto/userResponse';
+import { Error } from '../error';
 
 const userRepo = (db: DataSource) => ({
-	create: async (u: User): Promise<UserResponse> => {
+	create: async (u: User) => {
+		try {
 		await db.manager.save(u);
 		userResponse.id = u.id;
 		userResponse.username = u.username;
 		userResponse.type = u.type;
 		return userResponse;
+		} catch (e: any) {
+			console.log(e);
+			Error.exec('Database error', 500);
+			return userResponse;
+		}
 	}
 })
 
