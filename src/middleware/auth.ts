45795a46ed3b34e6753dc74b9998d8f2e5afde2d
vincent villaluna <villalunavincent@gmail.com>
2022-07-20 20:11:52 +0800

    Add authorization middleware

---
+++ src/middleware/auth.ts
@@ -0,0 +1,19 @@
+import { Response, NextFunction } from 'express';
+import { jwt } from '../util/jwt';
+
+const authorize = (req: any, res: Response, next: NextFunction) => {
+	try {
+	const authHeader = req.headers.authorization;
+	const token = authHeader?.split(' ')[1];
+
+	const user = jwt.verify(token);
+
+	req.user = user;
+	
+	next();
+	} catch (e: any) {
+		res.status(403).json({ message: 'Authorization required!'});
+	}
+}
+
+export default authorize;
