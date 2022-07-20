ef4fdf817e40b7aa5e71a1f82be100c664b21159
vincent villaluna <villalunavincent@gmail.com>
2022-07-20 14:30:41 +0800

    Hash password before saving to database

--- src/usecase/userUsecase.ts
+++ src/usecase/userUsecase.ts
@@ -1,13 +1,14 @@
 import UserRepo from '../repo/user';
 import { UserRequest } from '../dto/userRequest';
 import { User } from '../entity/User';
+import { hash } from '../util/hasher';
 
 const userUsecase = (repo: UserRepo) => ({
 	create: async (u: UserRequest) => {
 		u.validate()
 		const user = new User();
 		user.username = u.username;
-		user.password = u.password;
+		user.password = await hash(u.password);
 		user.type = u.type;
 		const userResponse = await repo.create(user);
 		return userResponse
