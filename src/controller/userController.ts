adbef93e27033022d972c29e76764f896f8977d6
vincent villaluna <villalunavincent@gmail.com>
2022-07-20 13:35:02 +0800

    Add error handling

--- src/controller/userController.ts
+++ src/controller/userController.ts
@@ -3,11 +3,17 @@ import UserUsecase from '../usecase/user';
 import userRequest from '../dto/userRequest';
 
 const userController = (uc: UserUsecase) => async (req: Request, res: Response) => {
+	try {
 	userRequest.username = req.body.username;
 	userRequest.password = req.body.password;
 
 	const user = await uc.create(userRequest);
 	res.status(200).json(user);
+	} catch (e: any) {
+		res.status(e.code).json(e.message);
+	} finally {
+		res.status(500).json({ message: 'Server error 500' });
+	}
 }
 
 export default userController;
