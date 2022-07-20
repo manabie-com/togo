79ebc5250fc80fec9f1ab4e51a4aac0d9573bc3d
vincent villaluna <villalunavincent@gmail.com>
2022-07-20 16:38:02 +0800

    Restructure controller to object and authenticate handler

--- src/controller/userController.ts
+++ src/controller/userController.ts
@@ -2,16 +2,30 @@ import { Request, Response } from 'express';
 import UserUsecase from '../usecase/user';
 import userRequest from '../dto/userRequest';
 
-const userController = (uc: UserUsecase) => async (req: Request, res: Response) => {
-	try {
-	userRequest.username = req.body.username;
-	userRequest.password = req.body.password;
+const userController = (uc: UserUsecase) => ({
+	create: async (req: Request, res: Response) => {
+		try {
+			userRequest.username = req.body.username;
+			userRequest.password = req.body.password;
 
-	const user = await uc.create(userRequest);
-	res.status(200).json(user);
-	} catch (e: any) {
-		res.status(e.code).json(e.message);
+			const user = await uc.create(userRequest);
+			res.status(200).json(user);
+		} catch (e: any) {
+			res.status(e.code).json(e.message);
+		}
+	},
+	authenticate: async (req: Request, res: Response) => {
+		try {
+			userRequest.username = req.body.username;
+			userRequest.password = req.body.password;
+
+			const token = await uc.authenticate(userRequest);
+
+			res.status(200).json(token);
+		} catch (e: any) {
+			res.status(e.code).json(e.message);
+		}
 	}
-}
+})
 
 export default userController;
