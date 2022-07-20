a7f30744a2a5f45d66afbe6ff93909b1f37f3d5a
vincent villaluna <villalunavincent@gmail.com>
2022-07-20 10:56:45 +0800

    Use env variables

--- src/db.ts
+++ src/db.ts
@@ -3,11 +3,11 @@ import { User } from './entity/User';
 
 const opt: DataSourceOptions = {
 	type: 'mysql',
-	host: 'localhost',
+	host: process.env.DB_HOST,
 	port: 3306,
-	username: 'root',
-	password: 'password',
-	database: 'togo',
+	username: process.env.DB_USER,
+	password: process.env.DB_PASSWORD,
+	database: process.env.DB_NAME,
 	synchronize: true,
 	logging: false,
 	dropSchema: true,
