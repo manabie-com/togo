920cc33bbf2b74bb7d0c4d0de6a9d44262b054cb
vincent villaluna <villalunavincent@gmail.com>
2022-07-20 20:11:24 +0800

    Create Todo entity

---
+++ src/entity/Todo.ts
@@ -0,0 +1,19 @@
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn } from 'typeorm';
+
+@Entity()
+export class Todo {
+	@PrimaryGeneratedColumn()
+	id: number;
+
+	@Column()
+	userId: number;
+
+	@Column()
+	task: string;
+
+	@Column()
+	done: boolean;
+
+	@CreateDateColumn()
+	createdAt: Date;
+}
