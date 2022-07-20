aac0747d06531167be5c49bf49bc4fb3a6d86408
vincent villaluna <villalunavincent@gmail.com>
2022-07-20 13:37:00 +0800

    Append type and validate inputs

--- src/usecase/userUsecase.ts
+++ src/usecase/userUsecase.ts
@@ -4,9 +4,11 @@ import { User } from '../entity/User';
 
 const userUsecase = (repo: UserRepo) => ({
 	create: async (u: UserRequest) => {
+		u.validate()
 		const user = new User();
 		user.username = u.username;
 		user.password = u.password;
+		user.type = u.type;
 		const userResponse = await repo.create(user);
 		return userResponse
 	}
