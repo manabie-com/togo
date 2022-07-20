68fefd952738b9a69ec491cccf2ad0e9c6a0abb8
vincent villaluna <villalunavincent@gmail.com>
2022-07-20 13:35:51 +0800

    Add type to know which type of user

--- src/dto/userResponse.ts
+++ src/dto/userResponse.ts
@@ -1,11 +1,13 @@
 export interface UserResponse {
 	id: number;
 	username: string;
+	type: string;
 }
 
 const userResponse: UserResponse = {
 	id: 0,
 	username: '',
+	type: '',
 }
 
 export default userResponse;
