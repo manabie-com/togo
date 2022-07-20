cfa76214db48f7b94aea2a79f979a2595856a611
vincent villaluna <villalunavincent@gmail.com>
2022-07-20 13:35:26 +0800

    Add data validation

--- src/dto/userRequest.ts
+++ src/dto/userRequest.ts
@@ -1,11 +1,21 @@
+import { Error } from '../error'
+
 export interface UserRequest {
 	username: string;
 	password: string;
+	type: string;
+	validate(): void;
 }
 
 const userRequest: UserRequest = {
 	username: '',
 	password: '',
+	type: 'basic',
+	validate() {
+		if (!this.username || this.username == '') Error.exec(`Username is invalid: ${this.username}`, 400)
+		if (!this.password || this.password == '') Error.exec('Password is invalid', 400)
+		if (!this.type || this.type == '') Error.exec('Type is invalid it should be "basic" or "premium"', 400)
+	}
 }
 
 export default userRequest;
