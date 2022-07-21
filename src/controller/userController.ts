99edba7d1e72a4ca31d99aad43618e1146abbe31
vincent villaluna <villalunavincent@gmail.com>
2022-07-21 12:34:17 +0800

    Return response for testing requirements

--- src/controller/userController.ts
+++ src/controller/userController.ts
@@ -14,24 +14,24 @@ const userController = (uc: UserUsecase | any) => ({
 			res.status(e.code).json(e.message);
 		}
 	},
-	authenticate: async (req: Request, res: Response) => {
+	authenticate: async (req: Request | any, res: Response | any) => {
 		try {
 			userRequest.username = req.body.username;
 			userRequest.password = req.body.password;
 
 			const token = await uc.authenticate(userRequest);
 
-			res.status(200).json(token);
+			return res.status(200).json(token);
 		} catch (e: any) {
 			res.status(e.code).json(e.message);
 		}
 	},
-	upgrade: async (req: any, res: Response) => {
+	upgrade: async (req: any, res: Response | any) => {
 		try {
 			userRequest.id = req.user.id;
 
 			const user = await uc.upgrade(userRequest);
-			res.status(200).json(user);
+			return res.status(200).json(user);
 		} catch (e: any) {
 			res.status(e.code).json(e.message);
 		}
