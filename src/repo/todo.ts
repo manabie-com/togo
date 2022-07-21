ead295a017bd4abcc49fc032b5c322672c28708e
vincent villaluna <villalunavincent@gmail.com>
2022-07-21 11:54:16 +0800

    Create signature for getting todos

--- src/repo/todo.ts
+++ src/repo/todo.ts
@@ -1,7 +1,11 @@
 import { Todo } from '../entity/Todo';
+import { User } from '../entity/User';
 
 interface TodoRepo {
 	create(t: Todo): Promise<boolean>
+	getAll(): Promise<Todo[]>
+	getAllByUser(u: User): Promise<Todo[]>
+	getTodayByUser(u: User): Promise<Todo[]>
 }
 
 export default TodoRepo;
