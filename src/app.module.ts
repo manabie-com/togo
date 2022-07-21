3b01c0268dff4629ea4cb693df44fd37ec6c082c
Nguyen Nghia <nghiand@devblock.net>
2022-07-21 09:51:10 +0700

    generate user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TodosModule } from './todos/todos.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -22,6 +23,7 @@ import { TodosModule } from './todos/todos.module';
       logging: true
     }),
     TodosModule,
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
