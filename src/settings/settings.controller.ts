e653acba417be05e71470d432657f532da18fac0
Nguyen Nghia <nghiand@devblock.net>
2022-07-21 15:39:43 +0700

    generate user setting module

---
+++ src/settings/settings.controller.ts
@@ -0,0 +1,34 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { SettingsService } from './settings.service';
+import { CreateSettingDto } from './dto/create-setting.dto';
+import { UpdateSettingDto } from './dto/update-setting.dto';
+
+@Controller('settings')
+export class SettingsController {
+  constructor(private readonly settingsService: SettingsService) {}
+
+  @Post()
+  create(@Body() createSettingDto: CreateSettingDto) {
+    return this.settingsService.create(createSettingDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.settingsService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.settingsService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateSettingDto: UpdateSettingDto) {
+    return this.settingsService.update(+id, updateSettingDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.settingsService.remove(+id);
+  }
+}
---
+++ src/settings/settings.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@nestjs/common';
+import { CreateSettingDto } from './dto/create-setting.dto';
+import { UpdateSettingDto } from './dto/update-setting.dto';
+
+@Injectable()
+export class SettingsService {
+  create(createSettingDto: CreateSettingDto) {
+    return 'This action adds a new setting';
+  }
+
+  findAll() {
+    return `This action returns all settings`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} setting`;
+  }
+
+  update(id: number, updateSettingDto: UpdateSettingDto) {
+    return `This action updates a #${id} setting`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} setting`;
+  }
+}
