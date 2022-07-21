3931326a0beaf96fe93614d84892e9316d851b95
vincent villaluna <villalunavincent@gmail.com>
2022-07-21 11:59:04 +0800

    Add constant variables

---
+++ src/util/constant.ts
@@ -0,0 +1,13 @@
+export const CONSTANTS = {
+	ACCOUNT_TYPE: {
+		BASIC: 'BASIC',
+		PREMIUM: 'PREMIUM',
+		BASICLIMIT: 5,
+		PREMIUMLIMIT: 10,
+		GETDAILYLIMIT: function(type: string) {
+			if (this.BASIC === type) return this.BASICLIMIT;
+			if (this.PREMIUM === type) return this.PREMIUMLIMIT;
+			return 0;
+		}
+	},
+}
