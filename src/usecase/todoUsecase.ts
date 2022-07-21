4dabd4dca2eb88972b14d5ea3b95566c721ca31b
vincent villaluna <villalunavincent@gmail.com>
2022-07-21 11:57:12 +0800

    Implement get todos from usecase

--- src/usecase/todoUsecase.ts
+++ src/usecase/todoUsecase.ts
@@ -1,10 +1,23 @@
 import TodoRepo from '../repo/todo';
 import { TodoRequest } from '../dto/todoRequest';
+import { UserRequest } from '../dto/userRequest';
 import { Todo } from '../entity/Todo';
+import { User } from '../entity/User';
+import { CONSTANTS } from '../util/constant';
+import { Error } from '../error';
 
 const todoUsecase = (repo: TodoRepo) => ({
 	create: async (t: TodoRequest) => {
 		t.validate();
+
+		const user = new User();
+		user.id = t.userId;
+
+		const userResponse = await repo.getTodayByUser(user);
+
+		if (userResponse.length >= CONSTANTS.ACCOUNT_TYPE.GETDAILYLIMIT(t.userType)) {
+			Error.exec('You have reached your daily limit', 400)
+		}
 
 		const todo = new Todo()
 
@@ -15,6 +28,17 @@ const todoUsecase = (repo: TodoRepo) => ({
 		const todoResponse = await repo.create(todo);
 		return todoResponse;
 	},
+	getAll: async() => {
+		const todosResponse = await repo.getAll();
+		return todosResponse;
+	},
+	getAllByUser: async(u: UserRequest) => {
+		const user = new User();
+		user.id = u.id;
+
+		const todosResponse = await repo.getAllByUser(user);
+		return todosResponse;
+	}
 });
 
 export default todoUsecase;
