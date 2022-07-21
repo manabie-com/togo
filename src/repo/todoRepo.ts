5890e3c3ad4cd2cbd3610c3f915fccef06ea170c
vincent villaluna <villalunavincent@gmail.com>
2022-07-21 11:54:46 +0800

    Implement get todos for repository

--- src/repo/todoRepo.ts
+++ src/repo/todoRepo.ts
@@ -1,5 +1,6 @@
 import { DataSource } from 'typeorm';
 import { Todo } from '../entity/Todo';
+import { User } from '../entity/User';
 import { Error } from '../error';
 
 const todoRepo = (db: DataSource) => ({
@@ -13,6 +14,43 @@ const todoRepo = (db: DataSource) => ({
 			return false;
 		}
 	},
+	getAll: async () => {
+		try {
+			const repo = db.getRepository(Todo);
+			const todos = await repo.find();
+			return todos;
+		} catch (e: any) {
+			const result: Todo[] = [];
+			Error.exec('Database error', 500);
+			return result;
+		}
+	},
+	getAllByUser: async (u: User) => {
+		try {
+			const repo = db.getRepository(Todo);
+			const todos = await repo.findBy({ userId: u.id });
+			return todos;
+		} catch (e: any) {
+			const result: Todo[] = [];
+			Error.exec('Database error', 500);
+			return result;
+		}
+	},
+	getTodayByUser: async (u: User) => {
+		try {
+			const repo = db.getRepository(Todo);
+			const todos = await repo.createQueryBuilder('todo')
+			.where('todo.createdAt >= CURDATE() AND todo.createdAt <= DATE_ADD(CURDATE(), INTERVAL 1 DAY)')
+			.andWhere('todo.userId = :id', {id: u.id})
+			.getRawMany();
+			return todos;
+
+		} catch (e: any) {
+			const result: Todo[] = [];
+			Error.exec('Database error', 500);
+			return result;
+		}
+	}
 })
 
 export default todoRepo;
