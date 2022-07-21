a778edf089b5bc8a06d6c1668123571e548aef62
vincent villaluna <villalunavincent@gmail.com>
2022-07-21 11:56:46 +0800

    Add signature for get todos from usecase

--- src/usecase/todo.ts
+++ src/usecase/todo.ts
@@ -1,7 +1,11 @@
 import { TodoRequest } from '../dto/todoRequest';
+import { UserRequest } from '../dto/userRequest';
+import { Todo } from '../entity/Todo';
 
 interface TodoUsecase {
 	create(t: TodoRequest): Promise<boolean>
+	getAll(): Promise<Todo[]>
+	getAllByUser(u: UserRequest): Promise<Todo[]>
 }
 
 export default TodoUsecase;
