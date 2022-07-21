62456b73c1c4a8e41cbc355dc99a29b8b725a496
Patrick Santos <pgsantos.92@gmail.com>
2022-07-22 05:26:56 +0800

    Make App respond to a GET request to root rout

--- app/app.go
+++ app/app.go
@@ -1,3 +1,18 @@
 package app
 
-type App struct{}
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type App struct {
+	Router *mux.Router
+}
+
+func (app *App) Initialize() {
+	app.Router = mux.NewRouter()
+	app.Router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	}).Methods("GET")
+}
--- tests/app_test.go
+++ tests/app_test.go
@@ -1,6 +1,8 @@
 package main_test
 
 import (
+	"net/http"
+	"net/http/httptest"
 	"reflect"
 	"testing"
 	"togo/app"
@@ -12,5 +14,17 @@ func TestApp_TypeShouldExist(t *testing.T) {
 
 	if appVarType.Name() != "App" && appVarType.PkgPath() == "" {
 		t.Error("Type does not exist")
+	}
+}
+
+func TestApp_ShouldRespondToBasicGetRequest(t *testing.T) {
+	app := app.App{}
+	app.Initialize()
+	request, _ := http.NewRequest("GET", "/", nil)
+	responseRecorder := httptest.NewRecorder()
+	app.Router.ServeHTTP(responseRecorder, request)
+
+	if http.StatusOK != responseRecorder.Code {
+		t.Errorf("Expected response code %d. Got %d\n", http.StatusOK, responseRecorder.Code)
 	}
 }
