657c007448247a49e476cd7d74e572e3ff702c6b
Dien DUONG <hi@duongludien.com>
2022-08-06 09:57:47 +0700

    fix: crazy dll references

--- src/Togo.Api.IntegrationTests/UserControllerTest.cs
+++ src/Togo.Api.IntegrationTests/UserControllerTest.cs
@@ -1,8 +1,4 @@
-using System;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.Testing;
-using Togo.Infrastructure.Identities.Dtos;
-using Xunit;
 
 namespace Togo.Api.IntegrationTests;
 
@@ -10,20 +6,5 @@ public class UserControllerTest : BaseIntegrationTestClass
 {
     public UserControllerTest(WebApplicationFactory<Program> factory) : base(factory)
     {
-    }
-
-    [Fact]
-    public async Task CreateAsync_WhenAllCorrect_ShouldSuccess()
-    {
-        var createUserInput = new CreateUserDto
-        {
-            UserName = Guid.NewGuid().ToString(),
-            Password = "Abcd@1234",
-            MaxTasksPerDay = 10
-        };
-
-        var response = await PostAsync("api/user", createUserInput);
-        
-        Assert.True(response.IsSuccessStatusCode);
     }
 }
